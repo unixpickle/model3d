70cb9a213d1936b53c18f41bd4a95753e808eb12
Alex Nichol <unixpickle@gmail.com>
2019-09-29 12:55:33 -0400

    benchmark multi-step blur

---
+++ mesh_test.go
@@ -0,0 +1,18 @@
+package model3d
+
+import (
+	"math"
+	"testing"
+)
+
+func BenchmarkMeshBlur(b *testing.B) {
+	m := NewMeshPolar(func(g GeoCoord) float64 {
+		return 3 + math.Cos(g.Lat)*math.Sin(g.Lon)
+	}, 100)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < 7; i++ {
+			m.Blur(0.8)
+		}
+	}
+}
