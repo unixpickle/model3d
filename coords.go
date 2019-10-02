b387f2afa4916fb02ff6a9ebfce5b78a5b1fc9a2
Alex Nichol <unixpickle@gmail.com>
2019-10-02 14:26:26 -0400

    utility to render a grid of images

--- coords.go
+++ coords.go
@@ -154,6 +154,11 @@ func (c Coord3D) Max(c1 Coord3D) Coord3D {
 	return Coord3D{math.Max(c.X, c1.X), math.Max(c.Y, c1.Y), math.Max(c.Z, c1.Z)}
 }
 
+// Normalize gets a unit vector from c.
+func (c Coord3D) Normalize() Coord3D {
+	return c.Scale(1 / c.Norm())
+}
+
 func (c Coord3D) array() [3]float64 {
 	return [3]float64{c.X, c.Y, c.Z}
 }
--- render.go
+++ render.go
@@ -4,6 +4,7 @@ import (
 	"image"
 	"image/color"
 	"math"
+	"math/rand"
 )
 
 // RenderRayCast renders a Collider as a grayscale image
@@ -41,4 +42,49 @@ func RenderRayCast(c Collider, output *image.Gray, origin, x, y, z Coord3D, fov 
 			}
 		}
 	}
+}
+
+// RenderRandomGrid renders a collider from various random
+// angles and saves the images into a grid.
+func RenderRandomGrid(c Collider, rows, cols, thumbWidth, thumbHeight int) *image.Gray {
+	min := c.Min()
+	max := c.Max()
+	center := max.Add(min).Scale(0.5)
+	diff := max.Add(min.Scale(-1))
+	radius := math.Sqrt(3) * math.Max(diff.X, math.Max(diff.Y, diff.Z)) / 2
+
+	output := image.NewGray(image.Rect(0, 0, cols*thumbWidth, rows*thumbHeight))
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			z := (Coord3D{
+				X: rand.NormFloat64(),
+				Y: rand.NormFloat64(),
+				Z: rand.NormFloat64(),
+			}).Normalize()
+			origin := center.Add(z.Scale(-radius * 2))
+
+			// Use the derivatives with respect to lat/lon
+			// as the y and x axis.
+			geo := z.Scale(-1).Geo()
+			x := (GeoCoord{Lat: geo.Lat, Lon: geo.Lon + 1e-4}).Coord3D().Add(z)
+			y := (GeoCoord{Lat: geo.Lat + 1e-4, Lon: geo.Lon}).Coord3D().Add(z)
+
+			// Ensure that we get non-nan axes which are
+			// truly orthogonal, even for the poles.
+			x.X += 1e-8
+			y.X += 1e-8
+			x = x.Normalize()
+			y = y.Normalize()
+			x = x.Add(z.Scale(-x.Dot(z))).Normalize()
+			x = x.Normalize()
+			y = y.Add(z.Scale(-y.Dot(z)))
+			y = y.Add(x.Scale(-y.Dot(x)))
+
+			rect := image.Rect(j*thumbWidth, i*thumbHeight, (j+1)*thumbWidth, (i+1)*thumbHeight)
+			RenderRayCast(c, output.SubImage(rect).(*image.Gray), origin, x, y, z, math.Pi/2)
+		}
+	}
+
+	return output
 }
