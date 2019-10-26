b5bcfe4e9d7e059d75ee3066181866b78e124fd9
Alex Nichol <unixpickle@gmail.com>
2019-10-25 23:56:13 -0400

    pumpkin carving example

--- examples/pumpkin/main.go
+++ examples/pumpkin/main.go
@@ -1,42 +1,75 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"image/png"
 	"io/ioutil"
-	"log"
 	"math"
+	"os"
 
+	"github.com/unixpickle/essentials"
 	"github.com/unixpickle/model3d"
 )
 
 const Thickness = 0.9
 
 func main() {
-	pumpkin := PumpkinSolid()
-	base := LidSolid{Solid: pumpkin}
+	r, err := os.Open("etching.png")
+	essentials.Must(err)
+	defer r.Close()
+	etching, err := png.Decode(r)
+	essentials.Must(err)
+
+	pumpkin := &model3d.SubtractedSolid{
+		Positive: PumpkinSolid{Scale: 1},
+		Negative: PumpkinSolid{Scale: Thickness},
+	}
+	base := &model3d.SubtractedSolid{
+		Positive: LidSolid{Solid: pumpkin},
+		Negative: EtchSolid{
+			Radius: 1.6,
+			Height: 1.5,
+			Image:  etching,
+		},
+	}
 	lid := LidSolid{IsLid: true, Solid: model3d.JoinedSolid{pumpkin, StemSolid{}}}
 
-	log.Println("Creating base...")
-	mesh1 := model3d.SolidToMesh(base, 0.05, 1, 0.8, 5)
-	ioutil.WriteFile("base.stl", mesh1.EncodeSTL(), 0755)
+	colorFunc := func(t *model3d.Triangle) [3]float64 {
+		c := t[0]
+		if (PumpkinSolid{Scale: 1.025}).Contains(c) {
+			expectedNormal := t[0].Geo().Coord3D()
+			if math.Abs(expectedNormal.Dot(t.Normal())) > 0.5 {
+				return [3]float64{214.0 / 255, 143.0 / 255, 0}
+			} else {
+				return [3]float64{255.0 / 255, 206.0 / 255, 107.0 / 255}
+			}
+		}
+		return [3]float64{79.0 / 255, 53.0 / 255, 0}
+	}
 
-	log.Println("Creating lid...")
-	mesh2 := model3d.SolidToMesh(lid, 0.05, 1, 0.8, 5)
-	ioutil.WriteFile("lid.stl", mesh2.EncodeSTL(), 0755)
+	mesh := model3d.SolidToMesh(base, 0.05, 2, 0.8, 5)
+	mesh.AddMesh(model3d.SolidToMesh(lid, 0.05, 2, 0.8, 5))
+	fmt.Println(mesh.NeedsRepair())
+	ioutil.WriteFile("pumpkin.zip", mesh.EncodeMaterialOBJ(colorFunc), 0755)
 }
 
-func PumpkinSolid() model3d.Solid {
-	mesh := model3d.NewMeshPolar(func(g model3d.GeoCoord) float64 {
-		return 1 + 0.1*math.Abs(math.Sin(g.Lon*4)) + 0.5*math.Cos(g.Lat)
-	}, 100)
-	mesh.Iterate(func(t *model3d.Triangle) {
-		t1 := *t
-		for i, c := range t1 {
-			t1[i] = c.Scale(Thickness)
-		}
-		t1[0], t1[1] = t1[1], t1[0]
-		mesh.Add(&t1)
-	})
-	return model3d.NewColliderSolid(model3d.MeshToCollider(mesh))
+type PumpkinSolid struct {
+	Scale float64
+}
+
+func (p PumpkinSolid) Min() model3d.Coord3D {
+	return model3d.Coord3D{X: -p.Scale * 1.6, Y: -p.Scale * 1.6, Z: -p.Scale * 1.6}
+}
+
+func (p PumpkinSolid) Max() model3d.Coord3D {
+	return p.Min().Scale(-1)
+}
+
+func (p PumpkinSolid) Contains(c model3d.Coord3D) bool {
+	g := c.Geo()
+	r := p.Scale * (1 + 0.1*math.Abs(math.Sin(g.Lon*4)) + 0.5*math.Cos(g.Lat))
+	return c.Norm() <= r
 }
 
 type StemSolid struct{}
@@ -79,4 +112,32 @@ func (l LidSolid) Contains(c model3d.Coord3D) bool {
 	}
 	inLid := model3d.Coord2D{X: c.X, Y: c.Z}.Norm() < 0.7*(c.Y-coneCenter)
 	return inLid == l.IsLid && l.Solid.Contains(c)
+}
+
+type EtchSolid struct {
+	Image  image.Image
+	Radius float64
+	Height float64
+}
+
+func (e EtchSolid) Min() model3d.Coord3D {
+	return model3d.Coord3D{X: -e.Radius, Y: -e.Height, Z: -e.Radius}
+}
+
+func (e EtchSolid) Max() model3d.Coord3D {
+	return e.Min().Scale(-1)
+}
+
+func (e EtchSolid) Contains(c model3d.Coord3D) bool {
+	if c.Min(e.Min()) != e.Min() || c.Max(e.Max()) != e.Max() {
+		return false
+	}
+
+	xFrac := c.Geo().Lon/(math.Pi*2) + 0.5
+	yFrac := 1 - (c.Y+e.Height)/(e.Height*2)
+
+	x := int(math.Round(xFrac * float64(e.Image.Bounds().Dx())))
+	y := int(math.Round(yFrac * float64(e.Image.Bounds().Dy())))
+	r, _, _, _ := e.Image.At(x, y).RGBA()
+	return r < 0xffff/2
 }
