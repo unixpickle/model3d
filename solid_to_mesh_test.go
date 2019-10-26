6e83a01f96f6202391286b9f5a4d6983f56e6fad
Alex Nichol <unixpickle@gmail.com>
2019-10-26 10:23:19 -0400

    singular edge test

--- solid_to_mesh_test.go
+++ solid_to_mesh_test.go
@@ -5,31 +5,23 @@ import (
 	"testing"
 )
 
-func TestSolidToMeshValid(t *testing.T) {
-	solid := &SubtractedSolid{
-		Positive: PumpkinSolid{Scale: 1},
-		Negative: PumpkinSolid{Scale: 0.9},
-	}
-	mesh := SolidToMesh(solid, 0.1, 0, 0, 0)
+func TestSolidToMeshSingularEdges(t *testing.T) {
+	// Create an adversarial pumpkin mesh.
+	mesh := NewMeshPolar(func(g GeoCoord) float64 {
+		return 1 + 0.1*math.Abs(math.Sin(g.Lon*4)) + 0.5*math.Cos(g.Lat)
+	}, 30)
+	mesh.Iterate(func(t *Triangle) {
+		t1 := *t
+		for i, c := range t1 {
+			t1[i] = c.Scale(0.9)
+		}
+		t1[0], t1[1] = t1[1], t1[0]
+		mesh.Add(&t1)
+	})
+	collider := MeshToCollider(mesh)
+	solid := NewColliderSolid(collider)
+	mesh = SolidToMesh(solid, 0.1, 0, 0, 0)
 	if mesh.NeedsRepair() {
 		t.Fatal("mesh needs repair")
 	}
-}
-
-type PumpkinSolid struct {
-	Scale float64
-}
-
-func (p PumpkinSolid) Min() Coord3D {
-	return Coord3D{X: -p.Scale * 1.6, Y: -p.Scale * 1.6, Z: -p.Scale * 1.6}
-}
-
-func (p PumpkinSolid) Max() Coord3D {
-	return p.Min().Scale(-1)
-}
-
-func (p PumpkinSolid) Contains(c Coord3D) bool {
-	g := c.Geo()
-	r := p.Scale * (1 + 0.1*math.Abs(math.Sin(g.Lon*4)) + 0.5*math.Cos(g.Lat))
-	return c.Norm() <= r
 }
