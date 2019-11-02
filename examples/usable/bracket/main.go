5736087c1ebaaa24f123a3592cb189d0cff2bd3b
Alex Nichol <unixpickle@gmail.com>
2019-11-02 16:16:02 -0400

    print bracket sideways
    This is easier for an FDM 3D printer to produce.

--- examples/usable/bracket/main.go
+++ examples/usable/bracket/main.go
@@ -20,24 +20,24 @@ func main() {
 	solid := &model3d.SubtractedSolid{
 		Positive: model3d.JoinedSolid{
 			&model3d.RectSolid{
-				MaxVal: model3d.Coord3D{X: BracketSize, Y: BracketHeight, Z: BracketDepth},
+				MaxVal: model3d.Coord3D{X: BracketSize, Z: BracketHeight, Y: BracketDepth},
 			},
 			&model3d.RectSolid{
 				MinVal: model3d.Coord3D{X: BracketSize - (HolePadding+HoleRadius)*2},
 				MaxVal: model3d.Coord3D{
 					X: BracketSize,
-					Y: BracketHeight,
-					Z: HoleDepth,
+					Z: BracketHeight,
+					Y: HoleDepth,
 				},
 			},
 			&model3d.RectSolid{
-				MaxVal: model3d.Coord3D{X: BracketDepth, Y: BracketHeight, Z: BracketSize},
+				MaxVal: model3d.Coord3D{X: BracketDepth, Z: BracketHeight, Y: BracketSize},
 			},
 			&model3d.RectSolid{
-				MinVal: model3d.Coord3D{Z: BracketSize - (HolePadding+HoleRadius)*2},
+				MinVal: model3d.Coord3D{Y: BracketSize - (HolePadding+HoleRadius)*2},
 				MaxVal: model3d.Coord3D{
-					Z: BracketSize,
-					Y: BracketHeight,
+					Y: BracketSize,
+					Z: BracketHeight,
 					X: HoleDepth,
 				},
 			},
@@ -46,26 +46,26 @@ func main() {
 			&model3d.CylinderSolid{
 				P1: model3d.Coord3D{
 					X: BracketSize - (HolePadding + HoleRadius),
-					Y: BracketHeight / 2,
-					Z: 0,
+					Z: BracketHeight / 2,
+					Y: 0,
 				},
 				P2: model3d.Coord3D{
 					X: BracketSize - (HolePadding + HoleRadius),
-					Y: BracketHeight / 2,
-					Z: HoleDepth,
+					Z: BracketHeight / 2,
+					Y: HoleDepth,
 				},
 				Radius: HoleRadius,
 			},
 			&model3d.CylinderSolid{
 				P1: model3d.Coord3D{
 					X: 0,
-					Y: BracketHeight / 2,
-					Z: BracketSize - (HolePadding + HoleRadius),
+					Z: BracketHeight / 2,
+					Y: BracketSize - (HolePadding + HoleRadius),
 				},
 				P2: model3d.Coord3D{
 					X: HoleDepth,
-					Y: BracketHeight / 2,
-					Z: BracketSize - (HolePadding + HoleRadius),
+					Z: BracketHeight / 2,
+					Y: BracketSize - (HolePadding + HoleRadius),
 				},
 				Radius: HoleRadius,
 			},
