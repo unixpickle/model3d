e8f2b66cc6073a619ba136cbe7639fada506495e
Alex Nichol <unixpickle@gmail.com>
2019-11-08 11:28:55 -0500

    razor holder example

---
+++ examples/usable/razor_holder/README.md
@@ -0,0 +1,5 @@
+# Razor holder
+
+This is a 3D-printable object which can be stuck to the wall in a shower to hold a manual razor.
+
+![Rendering of razor holder](rendering.png)
---
+++ examples/usable/razor_holder/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/unixpickle/model3d"
+)
+
+const (
+	Width        = 2.5
+	StickyHeight = 1.2
+	HolderHeight = 0.3
+	HolderLength = 1.0
+	Thickness    = 0.2
+
+	GapWidth = 0.75
+)
+
+func main() {
+	solid := &model3d.SubtractedSolid{
+		Positive: model3d.JoinedSolid{
+			&model3d.RectSolid{
+				MaxVal: model3d.Coord3D{X: Width, Y: HolderLength, Z: Thickness},
+			},
+			&model3d.RectSolid{
+				MaxVal: model3d.Coord3D{X: Width, Y: Thickness, Z: StickyHeight},
+			},
+			&model3d.RectSolid{
+				MinVal: model3d.Coord3D{X: 0, Y: HolderLength, Z: 0},
+				MaxVal: model3d.Coord3D{X: Width, Y: HolderLength + Thickness,
+					Z: Thickness + HolderHeight},
+			},
+		},
+		Negative: &model3d.RectSolid{
+			MinVal: model3d.Coord3D{X: (Width - GapWidth) / 2, Y: Thickness * 2},
+			MaxVal: model3d.Coord3D{X: (Width + GapWidth) / 2, Y: Thickness + HolderLength,
+				Z: StickyHeight},
+		},
+	}
+	mesh := model3d.SolidToMesh(solid, 0.1, 3, 0, 0)
+	ioutil.WriteFile("razor_holder.stl", mesh.EncodeSTL(), 0755)
+	model3d.SaveRandomGrid("rendering.png", model3d.MeshToCollider(mesh), 3, 3, 200, 200)
+}
