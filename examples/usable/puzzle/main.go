4da55ea810b6c890da236fc2b1d48704f8396c6f
Alex Nichol <unixpickle@gmail.com>
2019-11-20 17:55:59 -0500

    puzzle that fits together with screws

--- examples/usable/puzzle/main.go
+++ examples/usable/puzzle/main.go
@@ -2,218 +2,174 @@ package main
 
 import (
 	_ "image/gif"
-	"io/ioutil"
 	"log"
 	"math"
+	"os"
 
-	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/model3d/toolbox3d"
+
 	"github.com/unixpickle/model3d"
 )
 
 const (
-	SquareSize       = 0.5
-	SquareDepth      = 0.1
-	SquarePoleLength = 0.18
-	SquarePoleRadius = 0.05
-	SquareHolderSize = 0.25
-	SquareResolution = 10
+	HolderSize      = 0.5
+	HolderThickness = 0.2
+	TrackSize       = HolderSize / math.Sqrt2
+	PieceSize       = HolderSize + TrackSize - 0.05
+	PieceBottomSize = TrackSize + 0.1
+	PieceThickness  = 0.2
 
-	BoardBorder         = 0.3
-	BoardInnerThickness = 0.1
-	BoardSpacing        = 0.04
+	ScrewRadius     = 0.1
+	ScrewSlack      = 0.015
+	ScrewGrooveSize = 0.04
 
-	PartSpacing = 0.04
+	BottomThickness = 0.3
+	TotalThickness  = 1.0
+	WallThickness   = 0.3
 )
 
-const MinSpacing = 0.03
-
-var DefaultColor = [3]float64{0.039, 0.729, 0.71}
-
 func main() {
-	log.Println("Creating board mesh...")
-	puzzle := CreateBoard()
-	log.Println("Creating piece mesh...")
-	square := CreateSquare()
+	if _, err := os.Stat("board.stl"); os.IsNotExist(err) {
+		log.Println("Creating board...")
+		mesh := model3d.SolidToMesh(BoardSolid(), 0.01, 0, -1, 5)
+		log.Println("Eliminating co-planar polygons...")
+		mesh = mesh.EliminateCoplanar(1e-8)
+		mesh.SaveGroupedSTL("board.stl")
+	}
 
-	log.Println("Combining meshes...")
-	borderCollider := model3d.MeshToCollider(puzzle)
+	if _, err := os.Stat("holder.stl"); os.IsNotExist(err) {
+		log.Println("Creating holder...")
+		mesh := model3d.SolidToMesh(HolderSolid(), 0.01, 1, -1, 5)
+		mesh.SaveGroupedSTL("holder.stl")
+	}
 
-	for i := 0; i < 4; i++ {
-		xDelta := float64(i+1)*BoardSpacing + float64(i)*SquareSize
-		for j := 0; j < 4; j++ {
-			if i == 0 && j == 0 {
-				continue
+	if _, err := os.Stat("piece_top.stl"); os.IsNotExist(err) {
+		log.Println("Creating piece top...")
+		mesh := model3d.SolidToMesh(PieceTopSolid(), 0.01, 1, -1, 5)
+		mesh.SaveGroupedSTL("piece_top.stl")
+	}
+
+	if _, err := os.Stat("piece_bottom.stl"); os.IsNotExist(err) {
+		log.Println("Creating piece bottom...")
+		mesh := model3d.SolidToMesh(PieceBottomSolid(), 0.01, 1, -1, 5)
+		mesh.SaveGroupedSTL("piece_bottom.stl")
+	}
+}
+
+func BoardSolid() model3d.Solid {
+	bottomSize := WallThickness*2 + HolderSize*4 + TrackSize*4
+	solid := model3d.JoinedSolid{
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{},
+			MaxVal: model3d.Coord3D{X: bottomSize, Y: bottomSize, Z: BottomThickness},
+		},
+
+		// Front and back sides
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{},
+			MaxVal: model3d.Coord3D{X: bottomSize, Y: WallThickness, Z: TotalThickness},
+		},
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{Y: bottomSize - WallThickness},
+			MaxVal: model3d.Coord3D{X: bottomSize, Y: bottomSize, Z: TotalThickness},
+		},
+
+		// Left and right sides
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{},
+			MaxVal: model3d.Coord3D{Y: bottomSize, X: WallThickness, Z: TotalThickness},
+		},
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{X: bottomSize - WallThickness},
+			MaxVal: model3d.Coord3D{Y: bottomSize, X: bottomSize, Z: TotalThickness},
+		},
+	}
+
+	// Edge holders which can be built in to the board.
+	for x := 0; x < 5; x++ {
+		for y := 0; y < 5; y++ {
+			if x == 0 || x == 4 || y == 0 || y == 4 {
+				solid = append(solid, &model3d.RectSolid{
+					MinVal: model3d.Coord3D{
+						X: WallThickness - HolderSize/2 + float64(x)*(HolderSize+TrackSize),
+						Y: WallThickness - HolderSize/2 + float64(y)*(HolderSize+TrackSize),
+						Z: TotalThickness - HolderThickness - PieceThickness,
+					},
+					MaxVal: model3d.Coord3D{
+						X: WallThickness + HolderSize/2 + float64(x)*(HolderSize+TrackSize),
+						Y: WallThickness + HolderSize/2 + float64(y)*(HolderSize+TrackSize),
+						Z: TotalThickness - PieceThickness,
+					},
+				})
 			}
-			yDelta := float64(j+1)*BoardSpacing + float64(j)*SquareSize
-			delta := model3d.Coord3D{X: xDelta, Y: yDelta, Z: SquarePoleLength/2 + SquareDepth}
-			piece := square.MapCoords(delta.Add)
+		}
+	}
 
-			// Make sure the piece isn't too close to the border
-			// or they might fuse together during printing.
-			piece.Iterate(func(t *model3d.Triangle) {
-				for _, p := range t {
-					ray := &model3d.Ray{
-						Origin:    p,
-						Direction: model3d.Coord3D{X: 1.1, Y: 2.3, Z: 3.2},
-					}
-					if borderCollider.RayCollisions(ray)%2 == 1 ||
-						borderCollider.SphereCollision(p, MinSpacing) {
-						essentials.Die("undesired collision")
-					}
-				}
+	// Screw holes for holders.
+	screws := model3d.JoinedSolid{}
+	for x := 0; x < 3; x++ {
+		for y := 0; y < 3; y++ {
+			cx := WallThickness + float64(x+1)*(HolderSize+TrackSize)
+			cy := WallThickness + float64(y+1)*(HolderSize+TrackSize)
+			screws = append(screws, &toolbox3d.ScrewSolid{
+				P1:         model3d.Coord3D{X: cx, Y: cy, Z: TotalThickness - PieceThickness},
+				P2:         model3d.Coord3D{X: cx, Y: cy, Z: 0},
+				Radius:     ScrewRadius,
+				GrooveSize: ScrewGrooveSize,
 			})
-
-			puzzle.AddMesh(piece)
 		}
 	}
 
-	ioutil.WriteFile("puzzle.stl", puzzle.EncodeSTL(), 0755)
-}
-
-func CreateSquare() *model3d.Mesh {
-	m := model3d.NewMesh()
-
-	// Create the faces.
-	for i := 0; i < SquareResolution; i++ {
-		x1 := float64(i) / SquareResolution * SquareSize
-		x2 := float64(i+1) / SquareResolution * SquareSize
-		for j := 0; j < SquareResolution; j++ {
-			y1 := float64(j) / SquareResolution * SquareSize
-			y2 := float64(j+1) / SquareResolution * SquareSize
-			p1 := model3d.Coord3D{X: x1, Y: y1, Z: 0}
-			p2 := model3d.Coord3D{X: x2, Y: y1, Z: 0}
-			p3 := model3d.Coord3D{X: x2, Y: y2, Z: 0}
-			p4 := model3d.Coord3D{X: x1, Y: y2, Z: 0}
-
-			// Create top face.
-			m.Add(&model3d.Triangle{p1, p2, p3})
-			m.Add(&model3d.Triangle{p1, p3, p4})
-
-			// Create bottom face as well.
-			for _, p := range []*model3d.Coord3D{&p1, &p2, &p3, &p4} {
-				p.Z -= SquareDepth*2 + SquarePoleLength
-				frac := SquareHolderSize / SquareSize
-				p.X = SquareSize/2 + (p.X-SquareSize/2)*frac
-				p.Y = SquareSize/2 + (p.Y-SquareSize/2)*frac
-			}
-			m.Add(&model3d.Triangle{p1, p3, p2})
-			m.Add(&model3d.Triangle{p1, p4, p3})
-		}
-	}
-
-	// Create the remainder of the square.
-	m.Iterate(func(t *model3d.Triangle) {
-		for i := 0; i < 3; i++ {
-			p1 := t[i]
-			p2 := t[(i+1)%3]
-			if len(m.Find(p1, p2)) > 1 {
-				continue
-			}
-
-			// Create the side of the square.
-			p1, p2 = p2, p1
-			p3, p4 := p2, p1
-			if p1.Z == 0 {
-				p3.Z -= SquareDepth
-				p4.Z -= SquareDepth
-			} else {
-				p3.Z += SquareDepth
-				p4.Z += SquareDepth
-			}
-			m.Add(&model3d.Triangle{p1, p2, p3})
-			m.Add(&model3d.Triangle{p1, p3, p4})
-
-			// Create the part of the square that connects
-			// to the pole.
-			polePoint := func(p model3d.Coord3D) model3d.Coord3D {
-				angle := math.Atan2(p.Y-SquareSize/2, p.X-SquareSize/2)
-				// Hack to fix rounding errors and make all the
-				// vertices from the top and the bottom line up
-				// perfectly.
-				angle = math.Round(angle*123456.0) / 123456.0
-				return model3d.Coord3D{
-					X: math.Cos(angle)*SquarePoleRadius + SquareSize/2,
-					Y: math.Sin(angle)*SquarePoleRadius + SquareSize/2,
-					Z: p.Z,
-				}
-			}
-			if p1.Z == 0 {
-				p1, p2 = p4, p3
-			} else {
-				p1, p2 = p3, p4
-			}
-			p3 = polePoint(p2)
-			p4 = polePoint(p1)
-			m.Add(&model3d.Triangle{p1, p2, p3})
-			m.Add(&model3d.Triangle{p1, p3, p4})
-
-			// Create the pole.
-			if p1.Z > -(SquareDepth + SquarePoleLength - 1e-4) {
-				p1, p2 = p4, p3
-				p3, p4 = p2, p1
-				p3.Z = -(SquareDepth*2 + SquarePoleLength)
-				p4.Z = -(SquareDepth*2 + SquarePoleLength)
-				p3.Z += SquareDepth
-				p4.Z += SquareDepth
-				m.Add(&model3d.Triangle{p1, p2, p3})
-				m.Add(&model3d.Triangle{p1, p3, p4})
-			}
-		}
-	})
-
-	return m
-}
-
-func CreateBoard() *model3d.Mesh {
-	return model3d.SolidToMesh(BoardSolid{}, 0.05, 3, 0, 0).EliminateCoplanar(1e-8)
-}
-
-type BoardSolid struct{}
-
-func (b BoardSolid) Min() model3d.Coord3D {
-	return model3d.Coord3D{
-		X: -BoardBorder,
-		Y: -BoardBorder,
-		Z: -(SquarePoleLength/2 + SquareDepth + PartSpacing + BoardInnerThickness),
+	return &model3d.SubtractedSolid{
+		Positive: solid,
+		Negative: screws,
 	}
 }
 
-func (b BoardSolid) Max() model3d.Coord3D {
-	return model3d.Coord3D{
-		X: BoardBorder + BoardSpacing*5 + SquareSize*4,
-		Y: BoardBorder + BoardSpacing*5 + SquareSize*4,
-		Z: SquareDepth + SquarePoleLength/2,
+func HolderSolid() model3d.Solid {
+	center := HolderSize / 2
+	return model3d.JoinedSolid{
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{},
+			MaxVal: model3d.Coord3D{X: HolderSize, Y: HolderSize, Z: HolderThickness},
+		},
+		&toolbox3d.ScrewSolid{
+			P1:         model3d.Coord3D{X: center, Y: center, Z: 0},
+			P2:         model3d.Coord3D{X: center, Y: center, Z: TotalThickness - PieceThickness},
+			Radius:     ScrewRadius - ScrewSlack,
+			GrooveSize: ScrewGrooveSize,
+		},
 	}
 }
 
-func (b BoardSolid) Contains(p model3d.Coord3D) bool {
-	min := b.Min()
-	max := b.Max()
-	if min.Min(p) != min || max.Max(p) != max {
-		return false
+func PieceTopSolid() model3d.Solid {
+	center := PieceSize / 2
+	return model3d.JoinedSolid{
+		&model3d.RectSolid{
+			MinVal: model3d.Coord3D{},
+			MaxVal: model3d.Coord3D{X: PieceSize, Y: PieceSize, Z: PieceThickness},
+		},
+		&toolbox3d.ScrewSolid{
+			P1:         model3d.Coord3D{X: center, Y: center, Z: 0},
+			P2:         model3d.Coord3D{X: center, Y: center, Z: TotalThickness - BottomThickness},
+			Radius:     ScrewRadius - ScrewSlack,
+			GrooveSize: ScrewGrooveSize,
+		},
 	}
+}
 
-	if p.X < 0 || p.Y < 0 || p.X > max.X-BoardBorder || p.Y > max.Y-BoardBorder {
-		return true
+func PieceBottomSolid() model3d.Solid {
+	center := PieceBottomSize / 2
+	return &model3d.SubtractedSolid{
+		Positive: &model3d.RectSolid{
+			MinVal: model3d.Coord3D{},
+			MaxVal: model3d.Coord3D{X: PieceBottomSize, Y: PieceBottomSize, Z: PieceThickness},
+		},
+		Negative: &toolbox3d.ScrewSolid{
+			P1:         model3d.Coord3D{X: center, Y: center, Z: TotalThickness - BottomThickness},
+			P2:         model3d.Coord3D{X: center, Y: center, Z: 0},
+			Radius:     ScrewRadius,
+			GrooveSize: ScrewGrooveSize,
+		},
 	}
-
-	if p.Z < min.Z+BoardInnerThickness {
-		return true
-	}
-
-	for i := 0; i < 4; i++ {
-		x := float64(i)*SquareSize + float64(i+1)*BoardSpacing + SquareSize/2
-		radius := SquarePoleRadius + PartSpacing
-		if p.Z < -BoardInnerThickness/2 {
-			radius += (SquareSize - SquareHolderSize) / 2
-		}
-		if p.X > x-radius && p.X < x+radius {
-			return false
-		}
-		if p.Y > x-radius && p.Y < x+radius {
-			return false
-		}
-	}
-
-	return p.Z < BoardInnerThickness/2
 }
