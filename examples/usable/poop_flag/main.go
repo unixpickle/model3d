c468b6189b0fccb210bd7eecb62cea53c187404b
Alex Nichol <unixpickle@gmail.com>
2019-12-06 09:19:41 -0500

    steak for poop flag

--- examples/usable/poop_flag/main.go
+++ examples/usable/poop_flag/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"math"
 
+	"github.com/unixpickle/model3d/toolbox3d"
+
 	"github.com/unixpickle/model3d"
 )
 
@@ -11,12 +13,39 @@ const (
 	Height     = 1.2
 	RippleRate = math.Pi * 7
 	RippleSize = 0.02
+
+	ScrewRadius     = 0.3
+	ScrewGrooveSize = 0.05
+	ScrewSlack      = 0.03
+
+	SteakHeight = 3.0
 )
 
 func main() {
-	mesh := model3d.SolidToMesh(PoopSolid{}, 0.02, 1, -1, 5)
+	screw := &toolbox3d.ScrewSolid{
+		P1:         model3d.Coord3D{Z: -SteakHeight},
+		P2:         model3d.Coord3D{Z: Height / 2},
+		Radius:     ScrewRadius,
+		GrooveSize: ScrewGrooveSize,
+	}
+	pointedScrew := PointedSolid{
+		Solid:  screw,
+		Center: model3d.Coord3D{Z: Height / 2},
+	}
+	poop := &model3d.SubtractedSolid{
+		Positive: PoopSolid{},
+		Negative: pointedScrew,
+	}
+
+	mesh := model3d.SolidToMesh(poop, 0.02, 1, -1, 10)
 	mesh.SaveGroupedSTL("poop.stl")
 	model3d.SaveRandomGrid("rendering.png", model3d.MeshToCollider(mesh), 3, 3, 200, 200)
+
+	screw.P1, screw.P2 = screw.P2, screw.P1
+	screw.Radius -= ScrewSlack
+	mesh = model3d.SolidToMesh(pointedScrew, 0.01, 0, -1, 10)
+	mesh.SaveGroupedSTL("steak.stl")
+	model3d.SaveRandomGrid("steak.png", model3d.MeshToCollider(mesh), 3, 3, 200, 200)
 }
 
 type PoopSolid struct{}
@@ -37,4 +66,22 @@ func (p PoopSolid) Contains(c model3d.Coord3D) bool {
 	ripple := RippleSize * math.Sin(radius*RippleRate)
 	maxHeight := Height - (radius*Height/Radius + ripple)
 	return c.Z < maxHeight
+}
+
+type PointedSolid struct {
+	model3d.Solid
+	Center model3d.Coord3D
+}
+
+func (p PointedSolid) Contains(c model3d.Coord3D) bool {
+	if !p.Solid.Contains(c) {
+		return false
+	}
+	radius := p.Center.Z - c.Z
+	if radius < 0 {
+		return false
+	}
+	c1 := c.Sub(p.Center)
+	c1.Z = 0
+	return c1.Norm() <= radius
 }
