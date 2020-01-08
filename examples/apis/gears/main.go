364b6232e0cc42143a108c58f360e021971d3d09
Alex Nichol <unixpickle@gmail.com>
2020-01-08 15:26:10 -0800

    add gear demo

---
+++ examples/apis/gears/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"math"
+
+	"github.com/unixpickle/model3d"
+	"github.com/unixpickle/model3d/toolbox3d"
+)
+
+func main() {
+	solid := &toolbox3d.SpurGear{
+		P1:      model3d.Coord3D{},
+		P2:      model3d.Coord3D{Z: 0.2},
+		Profile: toolbox3d.InvoluteGearProfile(20*math.Pi/180, 0.1, 30),
+	}
+	mesh := model3d.SolidToMesh(solid, 0.003, 0, -1, 5)
+	mesh.SaveGroupedSTL("gear.stl")
+}
