6edf7dbf0d22bf5c936bcb6dff8fe38b1a027ddb
Alex Nichol <unixpickle@gmail.com>
2020-03-07 14:29:53 -0500

    start working on bigger drawers

--- examples/usable/drawer/main.go
+++ examples/usable/drawer/main.go
@@ -1,144 +1,135 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
+	"math"
 
 	"github.com/unixpickle/model3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 )
 
 const (
-	DrawerThickness = 0.2
-	DrawerHeight    = 1.0
-	DrawerWidth     = 2.0
-	DrawerDepth     = 2.0
+	DrawerWidth     = 6.0
+	DrawerHeight    = 2.0
+	DrawerDepth     = 6.0
+	DrawerSlack     = 0.05
+	DrawerThickness = 0.4
 
-	HandleLength     = 0.5
-	HandlePoleRadius = 0.2
-	HandleRadius     = 0.3
+	DrawerCount = 3
 
-	ContainerThickness = 0.3
-	GrooveSize         = 0.1
-	PartSpacing        = 0.03
+	ContainerThickness      = 0.2
+	ContainerFootWidth      = 0.6
+	ContainerFootHeight     = 0.2
+	ContainerFootRampHeight = ContainerFootWidth / 2
+
+	RidgeDepth = 0.2
 )
 
 func main() {
-	centerX := DrawerWidth/2 + GrooveSize + DrawerThickness
-	centerZ := (DrawerHeight + DrawerThickness) / 2
-	drawerWithHandle := model3d.JoinedSolid{
-		DrawerSolid{},
-		&model3d.CylinderSolid{
-			P1: model3d.Coord3D{
-				X: centerX,
-				Y: 0,
-				Z: centerZ,
-			},
-			P2: model3d.Coord3D{
-				X: centerX,
-				Y: -HandleLength,
-				Z: centerZ,
-			},
-			Radius: HandlePoleRadius,
-		},
-		&model3d.SphereSolid{
-			Center: model3d.Coord3D{
-				X: centerX,
-				Y: -HandleLength,
-				Z: centerZ,
-			},
-			Radius: HandleRadius,
-		},
-	}
-	log.Println("Creating drawer mesh...")
-	drawer := model3d.SolidToMesh(drawerWithHandle, PartSpacing, 2, 0.8, 3)
-	log.Println("Simplifying drawer mesh...")
-	drawer = drawer.EliminateCoplanar(1e-8)
+	container := CreateContainer()
 
 	log.Println("Creating container mesh...")
-	container := model3d.SolidToMesh(OuterSolid{}, PartSpacing, 2, 0.8, 3)
-	log.Println("Simplifying container mesh...")
-	container = container.EliminateCoplanar(1e-8)
-
-	log.Println("Combining and saving...")
-	drawer.AddMesh(container)
-	ioutil.WriteFile("drawer.stl", drawer.EncodeSTL(), 0755)
+	mesh := model3d.SolidToMesh(container, 0.02, 0, -1, 5)
+	log.Println("Eliminating co-planar polygons...")
+	mesh = mesh.EliminateCoplanar(1e-8)
+	log.Println("Saving container mesh...")
+	mesh.SaveGroupedSTL("container.stl")
+	log.Println("Rendering container mesh...")
+	model3d.SaveRandomGrid("shelf.png", model3d.MeshToCollider(mesh), 3, 3, 300, 300)
 }
 
-type DrawerSolid struct{}
+func CreateContainer() model3d.Solid {
+	var solid model3d.JoinedSolid
 
-func (d DrawerSolid) Min() model3d.Coord3D {
-	return model3d.Coord3D{}
+	// Side walls.
+	for _, x := range []float64{-ContainerThickness, DrawerWidth} {
+		solid = append(solid, &model3d.RectSolid{
+			MinVal: model3d.Coord3D{X: x, Z: -ContainerThickness},
+			MaxVal: model3d.Coord3D{
+				X: x + ContainerThickness,
+				Y: DrawerDepth + ContainerThickness,
+				Z: DrawerCount*DrawerHeight + ContainerThickness,
+			},
+		})
+	}
+
+	// Top/bottom walls.
+	for _, z := range []float64{-ContainerThickness, DrawerHeight * DrawerCount} {
+		solid = append(solid, &model3d.RectSolid{
+			MinVal: model3d.Coord3D{X: -ContainerThickness, Z: z},
+			MaxVal: model3d.Coord3D{
+				X: DrawerWidth + ContainerThickness,
+				Y: DrawerDepth + ContainerThickness,
+				Z: z + ContainerThickness,
+			},
+		})
+	}
+
+	// Back wall.
+	wallMin := solid.Min()
+	wallMin.Y = solid.Max().Y - ContainerThickness
+	solid = append(solid, &model3d.RectSolid{
+		MinVal: wallMin,
+		MaxVal: solid.Max(),
+	})
+
+	// Ridges for shelves.
+	for i := 0; i < DrawerCount; i++ {
+		for _, right := range []bool{false, true} {
+			solid = append(solid, CreateRidge((float64(i)+0.5)*DrawerHeight, right))
+		}
+	}
+
+	footXs := []float64{solid.Min().X + ContainerFootWidth/2, solid.Max().X - ContainerFootWidth/2}
+	footYs := []float64{solid.Min().Y + ContainerFootWidth/2, solid.Max().Y - ContainerFootWidth/2}
+	for _, x := range footXs {
+		for _, y := range footYs {
+			solid = append(solid, CreateFoot(x, y))
+		}
+	}
+
+	return solid
 }
 
-func (d DrawerSolid) Max() model3d.Coord3D {
-	return model3d.Coord3D{
-		X: DrawerWidth + DrawerThickness*2 + GrooveSize*2,
-		Y: DrawerDepth + DrawerThickness*2,
-		Z: DrawerHeight + DrawerThickness,
+func CreateRidge(z float64, onRight bool) model3d.Solid {
+	if !onRight {
+		return &RidgeSolid{X1: 0, X2: RidgeDepth, Z: z}
+	} else {
+		return &RidgeSolid{X1: DrawerWidth, X2: DrawerWidth - RidgeDepth, Z: z}
 	}
 }
 
-func (d DrawerSolid) Contains(c model3d.Coord3D) bool {
-	min := d.Min()
-	max := d.Max()
-	if min.Min(c) != min || max.Max(c) != max {
+type RidgeSolid struct {
+	X1 float64
+	X2 float64
+	Z  float64
+}
+
+func (r *RidgeSolid) Min() model3d.Coord3D {
+	return model3d.Coord3D{X: math.Min(r.X1, r.X2), Y: 0, Z: r.Z - RidgeDepth}
+}
+
+func (r *RidgeSolid) Max() model3d.Coord3D {
+	return model3d.Coord3D{X: math.Max(r.X1, r.X2), Y: DrawerDepth + ContainerThickness,
+		Z: r.Z + RidgeDepth}
+}
+
+func (r *RidgeSolid) Contains(c model3d.Coord3D) bool {
+	if !model3d.InSolidBounds(r, c) {
 		return false
 	}
-
-	if c.X >= GrooveSize && c.X <= DrawerThickness+GrooveSize {
-		return true
-	} else if c.X >= max.X-DrawerThickness-GrooveSize && c.X <= max.X-GrooveSize {
-		return true
-	} else if c.X <= GrooveSize || c.X >= max.X-GrooveSize {
-		center := max.Z / 2
-		return c.Z >= center-GrooveSize/2 && c.Z <= center+GrooveSize/2
-	} else if c.Z <= DrawerThickness {
-		return true
-	} else if c.Y <= DrawerThickness || c.Y >= max.Y-DrawerThickness {
-		return true
-	}
-
-	return false
+	return math.Abs(c.Z-r.Z) <= math.Abs(c.X-r.X2)
 }
 
-type OuterSolid struct{}
-
-func (o OuterSolid) Min() model3d.Coord3D {
-	return model3d.Coord3D{
-		X: GrooveSize - (PartSpacing + ContainerThickness),
-		Y: 0,
-		Z: -(PartSpacing + ContainerThickness),
+func CreateFoot(x, y float64) model3d.Solid {
+	center := model3d.Coord3D{X: x, Y: y, Z: -ContainerThickness}
+	halfSize := model3d.Coord3D{X: ContainerFootWidth / 2, Y: ContainerFootWidth / 2}
+	return &toolbox3d.Ramp{
+		Solid: &model3d.RectSolid{
+			MinVal: center.Sub(halfSize).Sub(model3d.Coord3D{Z: ContainerFootHeight}),
+			MaxVal: center.Add(halfSize),
+		},
+		P1: center.Sub(model3d.Coord3D{Z: ContainerFootRampHeight}),
+		P2: center,
 	}
-}
-
-func (o OuterSolid) Max() model3d.Coord3D {
-	delta := PartSpacing + ContainerThickness
-	mp := (DrawerSolid{}).Max()
-	mp.X += delta - GrooveSize
-	mp.Y += delta
-	mp.Z += delta
-	return mp
-}
-
-func (o OuterSolid) Contains(c model3d.Coord3D) bool {
-	min := o.Min()
-	max := o.Max()
-
-	if min.Min(c) != min || max.Max(c) != max {
-		return false
-	}
-
-	if c.Z <= min.Z+ContainerThickness || c.Z >= max.Z-ContainerThickness {
-		return true
-	} else if c.Y >= max.Y-ContainerThickness {
-		return true
-	} else if c.X <= min.X+ContainerThickness || c.X >= max.X-ContainerThickness {
-		center := (min.Z + max.Z) / 2
-		thickness := GrooveSize/2 + PartSpacing
-		return c.Z <= center-thickness || c.Z >= center+thickness ||
-			c.X <= min.X+ContainerThickness-GrooveSize ||
-			c.X >= max.X-ContainerThickness+GrooveSize
-	}
-
-	return false
 }
