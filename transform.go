19da43ffedc8defcdfa67c3c573e5f824ed12dc1
Alex Nichol <unixpickle@gmail.com>
2020-03-11 17:23:39 -0400

    matrix transformations

--- transform.go
+++ transform.go
@@ -49,6 +49,64 @@ func (t *translatedSolid) Contains(c Coord3D) bool {
 	return InSolidBounds(t, c) && t.solid.Contains(c.Sub(t.offset))
 }
 
+// Matrix3Transform is a Transform that applies a matrix
+// to coordinates.
+type Matrix3Transform struct {
+	Matrix *Matrix3
+}
+
+func (m *Matrix3Transform) Apply(c Coord3D) Coord3D {
+	return m.Matrix.MulColumn(c)
+}
+
+func (m *Matrix3Transform) ApplySolid(s Solid) Solid {
+	min := s.Min()
+	max := s.Max()
+	var newMin, newMax Coord3D
+	for i, x := range []float64{min.X, max.X} {
+		for j, y := range []float64{min.Y, max.Y} {
+			for k, z := range []float64{min.Z, max.Z} {
+				c := m.Matrix.MulColumn(Coord3D{X: x, Y: y, Z: z})
+				if i == 0 && j == 0 && k == 0 {
+					newMin, newMax = c, c
+				} else {
+					newMin = newMin.Min(c)
+					newMax = newMax.Max(c)
+				}
+			}
+		}
+	}
+	return &matrix3Solid{
+		inverse: m.Matrix.Inverse(),
+		min:     newMin,
+		max:     newMax,
+		solid:   s,
+	}
+}
+
+func (m *Matrix3Transform) Inverse() Transform {
+	return &Matrix3Transform{Matrix: m.Matrix.Inverse()}
+}
+
+type matrix3Solid struct {
+	inverse *Matrix3
+	min     Coord3D
+	max     Coord3D
+	solid   Solid
+}
+
+func (m *matrix3Solid) Min() Coord3D {
+	return m.min
+}
+
+func (m *matrix3Solid) Max() Coord3D {
+	return m.max
+}
+
+func (m *matrix3Solid) Contains(c Coord3D) bool {
+	return InSolidBounds(m, c) && m.solid.Contains(m.inverse.MulColumn(c))
+}
+
 // A JoinedTransform composes transformations from left to
 // right.
 type JoinedTransform []Transform
