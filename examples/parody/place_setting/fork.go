3775db8993e0c1b2ff63431e16e841b07bd608ad
Alex Nichol <unixpickle@gmail.com>
2020-04-08 15:28:15 -0400

    add fork to place setting

---
+++ examples/parody/place_setting/fork.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"math"
+
+	"github.com/unixpickle/model3d"
+	"github.com/unixpickle/model3d/model2d"
+)
+
+const (
+	ForkLength      = 5.0
+	ForkDip         = 0.5
+	ForkMaxWidth    = 1.0
+	ForkTipWidth    = 0.2
+	ForkThickness   = 0.1
+	ForkProngLength = 1.5 * ForkLength / 5
+	ForkNumProngs   = 4
+)
+
+type ForkSolid struct {
+	HeightCurve model2d.BezierCurve
+	WidthCurve  model2d.BezierCurve
+	MinVal      model3d.Coord3D
+	MaxVal      model3d.Coord3D
+}
+
+func NewForkSolid() *ForkSolid {
+	c := model2d.BezierCurve{
+		model2d.Coord{X: 0, Y: 0},
+		model2d.Coord{X: ForkProngLength, Y: -ForkDip * 2},
+		model2d.Coord{X: 3 * ForkLength / 5, Y: 0},
+		model2d.Coord{X: 3 * ForkLength / 5, Y: 0},
+		model2d.Coord{X: ForkLength, Y: 0},
+	}
+	var minZ float64
+	for t := 0.0; t <= 1.0; t += 1.0 / 1000 {
+		minZ = math.Min(minZ, c.Eval(t).Y)
+	}
+
+	wc := model2d.BezierCurve{
+		model2d.Coord{X: 0, Y: ForkMaxWidth / 2},
+		model2d.Coord{X: ForkProngLength, Y: ForkMaxWidth / 2},
+		model2d.Coord{X: ForkProngLength, Y: ForkMaxWidth / 2},
+		model2d.Coord{X: 1.1 * ForkProngLength, Y: ForkMaxWidth / 2},
+		model2d.Coord{X: 1.5 * ForkProngLength, Y: ForkTipWidth / 2},
+		model2d.Coord{X: 1.5 * ForkProngLength, Y: ForkTipWidth / 2},
+		model2d.Coord{X: ForkLength, Y: ForkTipWidth / 2},
+	}
+	return &ForkSolid{
+		HeightCurve: c,
+		WidthCurve:  wc,
+		MinVal:      model3d.Coord3D{X: 0, Y: -ForkMaxWidth / 2, Z: minZ},
+		MaxVal:      model3d.Coord3D{X: ForkLength, Y: ForkMaxWidth / 2, Z: ForkThickness},
+	}
+}
+
+func (f *ForkSolid) Min() model3d.Coord3D {
+	return f.MinVal
+}
+
+func (f *ForkSolid) Max() model3d.Coord3D {
+	return f.MaxVal
+}
+
+func (f *ForkSolid) Contains(c model3d.Coord3D) bool {
+	if !model3d.InBounds(f, c) {
+		return false
+	}
+	zMin := f.HeightCurve.EvalX(c.X)
+	if c.Z < zMin || c.Z > zMin+ForkThickness {
+		return false
+	}
+	width := f.WidthCurve.EvalX(c.X)
+	if math.Abs(c.Y) > width {
+		return false
+	}
+	if c.X < ForkProngLength {
+		prongWidth := width / 8 * math.Sqrt(1-(ForkProngLength-c.X)/ForkProngLength)
+
+		for i := 0.0; i < 4; i++ {
+			minY := -(width - width/8)
+			maxY := -minY
+			prongY := minY + i*(maxY-minY)/3
+			if math.Abs(c.Y-prongY) < prongWidth {
+				return true
+			}
+		}
+		return false
+	}
+	return true
+}
--- examples/parody/place_setting/main.go
+++ examples/parody/place_setting/main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	log.Println("Creating fork...")
+	fork := model3d.MarchingCubesSearch(NewForkSolid(), 0.01, 8)
+	fork.SaveGroupedSTL("fork.stl")
+
 	log.Println("Creating wine glass...")
 	wineGlass := model3d.MarchingCubesSearch(CreateWineGlass(), 0.02, 8)
 	wineGlass.SaveGroupedSTL("wine_glass.stl")
