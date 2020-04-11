ae9c8139c2eeb669fd5fdce6952173f012254120
Alex Nichol <unixpickle@gmail.com>
2020-04-11 16:53:50 -0400

    fidget_spinner: cleanup code & add README

---
+++ examples/usable/fidget_spinner/README.md
@@ -0,0 +1,9 @@
+# fidget-spinner
+
+This is a fidget spinner as a single interlocking part. It is intended to be printed using SLS or some other support-free method.
+
+The `PartSpacing` constant can be modified to give the axle more clearance. The default setting (0.01 inches) may be too agressive for most printers.
+
+# Rendering
+
+![Rendering of the model](rendering.png)
--- examples/usable/fidget_spinner/main.go
+++ examples/usable/fidget_spinner/main.go
@@ -2,159 +2,143 @@ package main
 
 import (
 	"fmt"
-	"image"
 	_ "image/jpeg"
-	"io/ioutil"
 	"log"
 	"math"
-	"os"
 
+	"github.com/unixpickle/model3d/model2d"
 	"github.com/unixpickle/model3d/render3d"
 
-	"github.com/unixpickle/essentials"
 	"github.com/unixpickle/model3d"
 )
 
 const (
-	AxleRadius  = 0.15
 	Thickness   = 0.15
 	PartSpacing = 0.01
 	EtchInset   = 0.05
+
+	AxleRadius   = 0.15
+	HolderRadius = 0.3
+	OrbitRadius  = 0.6
+	CircleRadius = 0.4
+	EtchRadius   = 0.4
 )
 
 func main() {
-	images := [3]image.Image{}
-	for i := range images {
-		f, err := os.Open(fmt.Sprintf("side_%d.png", i+1))
-		essentials.Must(err)
-		images[i], _, err = image.Decode(f)
-		f.Close()
-		essentials.Must(err)
-	}
-
-	axle := &model3d.SubtractedSolid{
-		Positive: model3d.JoinedSolid{
-			&model3d.Cylinder{
-				P1:     model3d.Coord3D{Z: -Thickness},
-				P2:     model3d.Coord3D{Z: Thickness},
-				Radius: AxleRadius,
-			},
-			&model3d.Cylinder{
-				P1:     model3d.Coord3D{Z: -Thickness},
-				P2:     model3d.Coord3D{Z: -Thickness * 1.5},
-				Radius: 0.3,
-			},
-			&model3d.Cylinder{
-				P1:     model3d.Coord3D{Z: Thickness},
-				P2:     model3d.Coord3D{Z: Thickness * 1.5},
-				Radius: 0.3,
-			},
-		},
-		Negative: model3d.JoinedSolid{
-			&model3d.Sphere{
-				Center: model3d.Coord3D{Z: -Thickness*1.5 - 1.0},
-				Radius: 1.05,
-			},
-			&model3d.Sphere{
-				Center: model3d.Coord3D{Z: Thickness*1.5 + 1.0},
-				Radius: 1.05,
-			},
-		},
-	}
-
-	mesh := model3d.MarchingCubesSearch(axle, 0.003, 8)
-
-	body := &model3d.SubtractedSolid{
-		Positive: model3d.JoinedSolid{
-			&model3d.Cylinder{
-				P1:     model3d.Coord3D{Z: -Thickness + PartSpacing},
-				P2:     model3d.Coord3D{Z: Thickness - PartSpacing},
-				Radius: 0.4,
-			},
-			&model3d.Cylinder{
-				P1:     model3d.Coord3D{Y: -0.6, Z: -Thickness + PartSpacing},
-				P2:     model3d.Coord3D{Y: -0.6, Z: Thickness - PartSpacing},
-				Radius: 0.4,
-			},
-			&model3d.Cylinder{
-				P1: model3d.Coord3D{
-					X: -0.6 * math.Sin(math.Pi*2/3),
-					Y: -0.6 * math.Cos(math.Pi*2/3),
-					Z: -Thickness + PartSpacing,
-				},
-				P2: model3d.Coord3D{
-					X: -0.6 * math.Sin(math.Pi*2/3),
-					Y: -0.6 * math.Cos(math.Pi*2/3),
-					Z: Thickness - PartSpacing,
-				},
-				Radius: 0.4,
-			},
-			&model3d.Cylinder{
-				P1: model3d.Coord3D{
-					X: -0.6 * math.Sin(math.Pi*4/3),
-					Y: -0.6 * math.Cos(math.Pi*4/3),
-					Z: -Thickness + PartSpacing,
-				},
-				P2: model3d.Coord3D{
-					X: -0.6 * math.Sin(math.Pi*4/3),
-					Y: -0.6 * math.Cos(math.Pi*4/3),
-					Z: Thickness - PartSpacing,
-				},
-				Radius: 0.4,
-			},
-		},
-		Negative: model3d.JoinedSolid{
-			&model3d.Cylinder{
-				P1:     model3d.Coord3D{Z: -Thickness + PartSpacing},
-				P2:     model3d.Coord3D{Z: Thickness - PartSpacing},
-				Radius: AxleRadius + PartSpacing,
-			},
-			&EtchedImage{
-				Image:  images[0],
-				X:      0,
-				Y:      -0.6,
-				Radius: 0.4,
-			},
-			&EtchedImage{
-				Image:  images[1],
-				X:      -0.6 * math.Sin(math.Pi*2/3),
-				Y:      -0.6 * math.Cos(math.Pi*2/3),
-				Radius: 0.4,
-			},
-			&EtchedImage{
-				Image:  images[2],
-				X:      -0.6 * math.Sin(math.Pi*4/3),
-				Y:      -0.6 * math.Cos(math.Pi*4/3),
-				Radius: 0.4,
-			},
-		},
-	}
-	mesh.AddMesh(model3d.MarchingCubesSearch(body, 0.003, 8))
+	log.Println("Creating axle mesh...")
+	mesh := model3d.MarchingCubesSearch(CreateAxle(), 0.003, 8)
+	log.Println("Creating body mesh...")
+	mesh.AddMesh(model3d.MarchingCubesSearch(CreateBody(), 0.003, 8))
+	log.Println("Post-processing mesh...")
 	mesh = mesh.SmoothAreas(0.05, 10)
+	mesh = mesh.EliminateCoplanar(1e-8)
 
-	ioutil.WriteFile("spinner.stl", mesh.EncodeSTL(), 0755)
+	log.Println("Saving mesh...")
+	mesh.SaveGroupedSTL("spinner.stl")
 
 	log.Println("Saving rendering of mesh...")
 	render3d.SaveRendering("rendering.png", mesh, model3d.Coord3D{Y: 2, Z: 2}, 400, 400, nil)
 }
 
+func CreateAxle() model3d.Solid {
+	return &model3d.SubtractedSolid{
+		Positive: model3d.JoinedSolid{
+			&model3d.Cylinder{
+				P1:     model3d.Coord3D{Z: -(Thickness + PartSpacing)},
+				P2:     model3d.Coord3D{Z: Thickness + PartSpacing},
+				Radius: AxleRadius - PartSpacing,
+			},
+			&model3d.Cylinder{
+				P1:     model3d.Coord3D{Z: -(Thickness + PartSpacing)},
+				P2:     model3d.Coord3D{Z: -(Thickness + PartSpacing) * 1.5},
+				Radius: HolderRadius,
+			},
+			&model3d.Cylinder{
+				P1:     model3d.Coord3D{Z: (Thickness + PartSpacing)},
+				P2:     model3d.Coord3D{Z: (Thickness + PartSpacing) * 1.5},
+				Radius: HolderRadius,
+			},
+		},
+		Negative: model3d.JoinedSolid{
+			&model3d.Sphere{
+				Center: model3d.Coord3D{Z: -(Thickness+PartSpacing)*1.5 - 1.0},
+				Radius: 1.05,
+			},
+			&model3d.Sphere{
+				Center: model3d.Coord3D{Z: (Thickness+PartSpacing)*1.5 + 1.0},
+				Radius: 1.05,
+			},
+		},
+	}
+}
+
+func CreateBody() model3d.Solid {
+	var circles model3d.JoinedSolid
+	for i, theta := range []float64{0, math.Pi * 2 / 3, math.Pi * 4 / 3} {
+		etching := NewEtchedImage(fmt.Sprintf("side_%d.png", i+1), theta)
+		circles = append(circles, &model3d.SubtractedSolid{
+			Positive: &model3d.Cylinder{
+				P1: model3d.Coord3D{
+					X: etching.X,
+					Y: etching.Y,
+					Z: -Thickness,
+				},
+				P2: model3d.Coord3D{
+					X: etching.X,
+					Y: etching.Y,
+					Z: Thickness,
+				},
+				Radius: CircleRadius,
+			},
+			Negative: etching,
+		})
+	}
+	return &model3d.SubtractedSolid{
+		// Add a middle circle to hold things together.
+		Positive: append(circles, &model3d.Cylinder{
+			P1:     model3d.Coord3D{Z: -Thickness},
+			P2:     model3d.Coord3D{Z: Thickness},
+			Radius: CircleRadius,
+		}),
+		// Hole for axle.
+		Negative: &model3d.Cylinder{
+			P1:     model3d.Coord3D{Z: -(Thickness + 1e-5)},
+			P2:     model3d.Coord3D{Z: Thickness + 1e-5},
+			Radius: AxleRadius,
+		},
+	}
+}
+
 type EtchedImage struct {
-	Image  image.Image
+	Solid  model2d.Solid
 	X      float64
 	Y      float64
 	Radius float64
 }
 
+func NewEtchedImage(path string, theta float64) *EtchedImage {
+	bmp := model2d.MustReadBitmap(path, nil)
+	mesh := bmp.Mesh().Smooth(10)
+	collider := model2d.MeshToCollider(mesh)
+	solid := model2d.NewColliderSolid(collider)
+	return &EtchedImage{
+		Solid:  model2d.ScaleSolid(solid, 1/float64(bmp.Width)),
+		X:      OrbitRadius * math.Sin(theta),
+		Y:      OrbitRadius * math.Cos(theta),
+		Radius: CircleRadius,
+	}
+}
+
 func (e *EtchedImage) Min() model3d.Coord3D {
-	return model3d.Coord3D{X: e.X - e.Radius, Y: e.Y - e.Radius, Z: -Thickness}
+	return model3d.Coord3D{X: e.X - e.Radius, Y: e.Y - e.Radius, Z: -(Thickness + 1e-5)}
 }
 
 func (e *EtchedImage) Max() model3d.Coord3D {
-	return model3d.Coord3D{X: e.X + e.Radius, Y: e.Y + e.Radius, Z: Thickness}
+	return model3d.Coord3D{X: e.X + e.Radius, Y: e.Y + e.Radius, Z: Thickness + 1e-5}
 }
 
 func (e *EtchedImage) Contains(c model3d.Coord3D) bool {
-	if c.X < e.X-e.Radius || c.Y < e.Y-e.Radius || c.X > e.X+e.Radius || c.Y > e.Y+e.Radius {
+	if !model3d.InBounds(e, c) {
 		return false
 	}
 	if math.Abs(c.Z) > Thickness || math.Abs(c.Z) < Thickness-EtchInset {
@@ -164,29 +148,16 @@ func (e *EtchedImage) Contains(c model3d.Coord3D) bool {
 	axis1 := model3d.Coord2D{X: e.X, Y: e.Y}
 	axis1 = axis1.Scale(1 / (axis1.Norm() * e.Radius))
 	axis2 := model3d.Coord2D{X: -axis1.Y, Y: axis1.X}
+
 	if c.Z < 0 {
+		// Flip image on under-side of spinner.
 		axis2 = axis2.Scale(-1)
 	}
+
 	p := model3d.Coord2D{X: c.X - e.X, Y: c.Y - e.Y}
 
-	x := int(math.Round(float64(e.Image.Bounds().Dx()) * (axis2.Dot(p) + 1) / 2))
-	y := int(math.Round(float64(e.Image.Bounds().Dy()) * (axis1.Dot(p) + 1) / 2))
+	x := (axis2.Dot(p) + 1) / 2
+	y := (axis1.Dot(p) + 1) / 2
 
-	if x < 0 {
-		return false
-	} else if x >= e.Image.Bounds().Dx() {
-		return false
-	}
-
-	if y < 0 {
-		return false
-	} else if y >= e.Image.Bounds().Dy() {
-		return false
-	}
-
-	r, _, _, _ := e.Image.At(x, y).RGBA()
-	if r > 0xffff/2 {
-		return false
-	}
-	return math.Abs(c.Z) > Thickness-EtchInset
+	return e.Solid.Contains(model2d.Coord{X: x, Y: y})
 }
