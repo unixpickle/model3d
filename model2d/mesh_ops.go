6fc90db347b97d478be9fff38e515cf5dee61432
Alex Nichol <unixpickle@gmail.com>
2020-04-12 15:20:00 -0400

    2d mesh subdivision
    This can be used to create a smoother mesh for a low-res image.

--- model2d/mesh_ops.go
+++ model2d/mesh_ops.go
@@ -33,6 +33,42 @@ func (m *Mesh) Smooth(iters int) *Mesh {
 	return im.Mesh()
 }
 
+// Subdivide uses Chaikin subdivision to add segments
+// between every vertex.
+//
+// This can only be applied to manifold meshes.
+// This can be checked with m.Manifold().
+func (m *Mesh) Subdivide(iters int) *Mesh {
+	if !m.Manifold() {
+		panic("mesh is non-manifold")
+	}
+	if iters < 1 {
+		panic("must subdivide for at least one iteration")
+	}
+	current := m
+	for i := 0; i < iters; i++ {
+		next := NewMesh()
+		current.Iterate(func(s *Segment) {
+			mp1 := s[0].Scale(0.75).Add(s[1].Scale(0.25))
+			mp2 := s[1].Scale(0.75).Add(s[0].Scale(0.25))
+			next.Add(&Segment{mp1, mp2})
+
+			// Do the subdivision for the second vertex.
+			// If every segment's second vertex is taken care of,
+			// every vertex will be covered.
+			others := current.Find(s[1])
+			s1 := others[0]
+			if s1 == s {
+				s1 = others[1]
+			}
+			mp3 := s1[0].Scale(0.75).Add(s1[1].Scale(0.25))
+			next.Add(&Segment{mp2, mp3})
+		})
+		current = next
+	}
+	return current
+}
+
 // Manifold checks if the mesh is manifold, i.e. if every
 // vertex has two segments.
 func (m *Mesh) Manifold() bool {
