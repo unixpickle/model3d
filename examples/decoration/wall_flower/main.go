1b6d1301914822d4467176e6b9a0955beedfab30
Alex Nichol <unixpickle@gmail.com>
2020-04-13 11:08:34 -0400

    wall_flower: cleanup code, smooth, add README

--- examples/decoration/wall_flower/main.go
+++ examples/decoration/wall_flower/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"image"
-	"image/png"
-	"os"
+	"log"
 
-	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/model3d/model2d"
+
 	"github.com/unixpickle/model3d"
 	"github.com/unixpickle/model3d/render3d"
 )
@@ -19,69 +18,87 @@ const (
 )
 
 func main() {
-	shape := NewFlowerShape()
-	sphereMesh := model3d.NewMeshPolar(func(g model3d.GeoCoord) float64 {
-		if shape.Get(g.Lat, g.Lon) {
-			return Radius
-		} else {
-			return 0
-		}
-	}, 500)
-	sphereMesh.Iterate(func(t *model3d.Triangle) {
-		if t[0].Norm() == 0 || t[1].Norm() == 0 || t[2].Norm() == 0 {
-			sphereMesh.Remove(t)
-		}
-	})
-	sphereMesh = sphereMesh.MapCoords(func(f model3d.Coord3D) model3d.Coord3D {
-		f.Z *= -1
-		f.Z += Radius
-		return f
-	})
+	log.Println("Wrapping flower shape...")
+	sphereMesh := WrappedFlowerShape()
+
+	log.Println("Creating solid...")
 	solid := model3d.JoinedSolid{
 		model3d.NewColliderSolidHollow(model3d.MeshToCollider(sphereMesh), Thickness),
 		BaseSolid{},
 	}
 
-	mesh := model3d.MarchingCubesSearch(solid, 0.01, 8).SmoothAreas(0.1, 20)
+	log.Println("Creating mesh...")
+	mesh := model3d.MarchingCubesSearch(solid, 0.01, 8)
+
+	log.Println("Saving mesh...")
 	mesh.SaveGroupedSTL("flower.stl")
+
+	log.Println("Rendering...")
 	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 300, nil)
 }
 
+func WrappedFlowerShape() *model3d.Mesh {
+	shape := NewFlowerShape()
+	mesh := model3d.NewMeshPolar(func(g model3d.GeoCoord) float64 {
+		if shape.Get(g.Lat, g.Lon) {
+			return Radius
+		} else {
+			return 0
+		}
+	}, 2000)
+	mesh.Iterate(func(t *model3d.Triangle) {
+		if t[0].Norm() == 0 || t[1].Norm() == 0 || t[2].Norm() == 0 {
+			mesh.Remove(t)
+		}
+	})
+	mesh = mesh.MapCoords(func(f model3d.Coord3D) model3d.Coord3D {
+		f.Z *= -1
+		f.Z += Radius
+		return f
+	})
+	return mesh
+}
+
 type FlowerShape struct {
-	Img image.Image
+	Solid model2d.Solid
 }
 
 func NewFlowerShape() *FlowerShape {
-	f, err := os.Open("shape.png")
-	essentials.Must(err)
-	defer f.Close()
-	img, err := png.Decode(f)
-	essentials.Must(err)
-	return &FlowerShape{Img: img}
+	bitmap := model2d.MustReadBitmap("shape.png", nil)
+	mesh := bitmap.Mesh().SmoothSq(200)
+	mesh = mesh.MapCoords(model2d.Coord{
+		X: 1 / float64(bitmap.Width),
+		Y: 1 / float64(bitmap.Height),
+	}.Mul)
+	collider := model2d.MeshToCollider(mesh)
+	solid := model2d.NewColliderSolid(collider)
+	return &FlowerShape{
+		Solid: solid,
+	}
 }
 
 func (f *FlowerShape) Get(x, y float64) bool {
-	intX := int(float64(f.Img.Bounds().Dx()) * (x + LengthRadians) / (LengthRadians * 2))
-	intY := int(float64(f.Img.Bounds().Dy()) * (y + LengthRadians) / (LengthRadians * 2))
-	_, _, _, a := f.Img.At(intX, intY).RGBA()
-	return a > 0xffff/2
+	return f.Solid.Contains(model2d.Coord{
+		X: (x + LengthRadians) / (LengthRadians * 2),
+		Y: (y + LengthRadians) / (LengthRadians * 2),
+	})
 }
 
 type BaseSolid struct{}
 
 func (b BaseSolid) Min() model3d.Coord3D {
-	return model3d.Coord3D{X: -Radius, Y: -Radius, Z: -BaseThickness}
+	return model3d.Coord3D{X: -BaseRadius, Y: -BaseRadius, Z: -BaseThickness}
 }
 
 func (b BaseSolid) Max() model3d.Coord3D {
-	return model3d.Coord3D{X: Radius, Y: Radius, Z: Radius}
+	return model3d.Coord3D{X: BaseRadius, Y: BaseRadius, Z: Radius}
 }
 
 func (b BaseSolid) Contains(c model3d.Coord3D) bool {
-	if c.Min(b.Min()) != b.Min() || c.Max(b.Max()) != b.Max() {
+	if !model3d.InBounds(b, c) {
 		return false
 	}
-	cylinderDist := (model3d.Coord3D{X: c.X, Y: c.Y}).Norm()
-	sphereDist := (model3d.Coord3D{X: c.X, Y: c.Y, Z: Radius - c.Z}).Norm()
+	cylinderDist := c.Coord2D().Norm()
+	sphereDist := c.Dist(model3d.Coord3D{Z: Radius})
 	return cylinderDist < BaseRadius && sphereDist >= Radius
 }
