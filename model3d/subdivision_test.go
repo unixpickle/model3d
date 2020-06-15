08ec07e69ea76fb6bb9c0ad886ed3bb9dc4559c5
Alex Nichol <unixpickle@gmail.com>
2020-06-15 17:50:41 -0400

    tests for subdivision

---
+++ model3d/subdivision_test.go
@@ -0,0 +1,43 @@
+package model3d
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestLoopSubdivision(t *testing.T) {
+	base := NewMeshRect(X(-1), Coord3D{X: 1, Y: 1, Z: 1})
+	mesh := LoopSubdivision(base, 4)
+
+	if mesh.NeedsRepair() {
+		t.Error("mesh needs repair")
+	}
+	if len(mesh.SingularVertices()) > 0 {
+		t.Error("mesh has singular vertices")
+	}
+	if _, n := mesh.RepairNormals(1e-8); n > 0 {
+		t.Error("mesh has bad normals")
+	}
+}
+
+func TestSubdivider(t *testing.T) {
+	subdiv := NewSubdivider()
+	mesh := NewMeshRect(X(-1), Coord3D{X: 1, Y: 1, Z: 1})
+	subdiv.AddFiltered(mesh, func(p1, p2 Coord3D) bool {
+		return rand.Intn(2) == 0
+	})
+	subdiv.Subdivide(mesh, func(p1, p2 Coord3D) Coord3D {
+		x, y := p2.Sub(p1).OrthoBasis()
+		return p1.Mid(p2).Add(x.Scale(1e-5 * rand.Float64())).Add(y.Scale(1e-5 * rand.Float64()))
+	})
+
+	if mesh.NeedsRepair() {
+		t.Error("mesh needs repair")
+	}
+	if len(mesh.SingularVertices()) > 0 {
+		t.Error("mesh has singular vertices")
+	}
+	if _, n := mesh.RepairNormals(1e-8); n > 0 {
+		t.Error("mesh has bad normals")
+	}
+}
