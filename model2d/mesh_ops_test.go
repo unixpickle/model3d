13d4d2864c5fcd043a59edc4b5bd11b718a9c7b3
Alex Nichol <unixpickle@gmail.com>
2020-06-17 16:05:25 -0400

    test model2d subdivision

--- model2d/mesh_ops_test.go
+++ model2d/mesh_ops_test.go
@@ -1,15 +1,13 @@
 package model2d
 
 import (
+	"math"
 	"math/rand"
 	"testing"
 )
 
 func TestMeshRepairNormals(t *testing.T) {
 	mesh := MarchingSquaresSearch(&Circle{Radius: 0.3}, 0.01, 8)
-	if !mesh.Manifold() {
-		t.Fatal("bad initial mesh")
-	}
 
 	mesh1, numRepairs := mesh.RepairNormals(1e-8)
 	if numRepairs > 0 {
@@ -37,6 +35,29 @@ func TestMeshRepairNormals(t *testing.T) {
 	}
 	if !meshesEqual(mesh, mesh1) {
 		t.Error("meshes are not equal")
+	}
+}
+
+func TestMeshSubdivide(t *testing.T) {
+	mesh := MarchingSquaresSearch(&Circle{Radius: 0.9}, 0.01, 8)
+	mesh1 := mesh.Subdivide(2)
+
+	if !mesh1.Manifold() {
+		t.Fatal("non-manifold subdivided mesh")
+	}
+	if _, n := mesh1.RepairNormals(1e-8); n != 0 {
+		t.Error("incorrect number of normals")
+	}
+
+	sdf := MeshToSDF(mesh)
+	sdf1 := MeshToSDF(mesh1)
+	for i := 0; i < 1000; i++ {
+		c := NewCoordRandNorm()
+		s1 := sdf.SDF(c)
+		s2 := sdf1.SDF(c)
+		if math.Abs(s1-s2) > 0.01 {
+			t.Errorf("bad SDF at %v: expected %f but got %f", c, s1, s2)
+		}
 	}
 }
 
