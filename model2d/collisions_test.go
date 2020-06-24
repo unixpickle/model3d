dd96afaef4ce601149ff92ea2e9b7f677caa29d6
Alex Nichol <unixpickle@gmail.com>
2020-06-24 16:35:44 -0400

    model2d: start adding collider tests

--- model2d/collisions_test.go
+++ model2d/collisions_test.go
@@ -5,6 +5,39 @@ import (
 	"testing"
 )
 
+func TestEmptyColliders(t *testing.T) {
+	// Make sure we don't get a crash from this.
+	GroupSegments(nil)
+
+	if MeshToCollider(NewMesh()).CircleCollision(Coord{}, 1000) {
+		t.Error("unexpected collision")
+	}
+}
+
+func TestSegmentCollisions(t *testing.T) {
+	segment := &Segment{Coord{X: 1, Y: 2}, Coord{X: 2, Y: 1}}
+	ray := &Ray{
+		Origin:    Coord{X: 0, Y: 1},
+		Direction: Coord{X: 1, Y: 0.5},
+	}
+	n := segment.RayCollisions(ray, func(rc RayCollision) {
+		if math.Abs(rc.Scale-1.3333333) > 0.0001 {
+			t.Error("unexpected scale")
+		}
+		if math.Abs(rc.Normal.X-0.7071) > 0.001 || math.Abs(rc.Normal.Y-0.7071) > 0.001 {
+			t.Error("unexpected normal")
+		}
+	})
+	if n != 1 {
+		t.Errorf("bad number of collisions")
+	}
+	ray1 := *ray
+	ray1.Direction = Coord{X: 1, Y: 1.01}
+	if segment.RayCollisions(&ray1, nil) != 0 {
+		t.Error("spurrious ray collision")
+	}
+}
+
 func BenchmarkMeshCollisions(b *testing.B) {
 	mesh := NewMesh()
 	for i := 0; i < 10000; i++ {
