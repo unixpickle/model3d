f996d173aa3943949952ddc5a238fa966ed516f0
Alex Nichol <unixpickle@gmail.com>
2020-07-09 19:30:03 -0400

    bezier curve regression test

--- model2d/bezier_test.go
+++ model2d/bezier_test.go
@@ -6,6 +6,46 @@ import (
 	"testing"
 )
 
+func TestBezierEval(t *testing.T) {
+	curves := []BezierCurve{
+		{
+			Coord{X: 1, Y: 3},
+			Coord{X: 2, Y: 2},
+			Coord{X: 2, Y: 3},
+			Coord{X: 3, Y: -2},
+			Coord{X: 2, Y: 3},
+		},
+		{
+			Coord{X: 3, Y: 3},
+			Coord{X: 2, Y: 2},
+			Coord{X: 2, Y: 3},
+			Coord{X: 1, Y: -2},
+		},
+	}
+	for i, c := range curves {
+		for j := 0; j < 100; j++ {
+			x := rand.Float64()
+			v1 := evalSimpleRecursive(c, x)
+			v2 := c.Eval(x)
+			if v1.Dist(v2) > 1e-5 {
+				t.Errorf("curve %d: time %f: expected %v but got %v", i, x, v1, v2)
+			}
+		}
+	}
+}
+
+func evalSimpleRecursive(b BezierCurve, t float64) Coord {
+	if len(b) < 2 {
+		panic("need at least two points")
+	}
+	if len(b) == 2 {
+		return b[0].Scale(1 - t).Add(b[1].Scale(t))
+	}
+	term1 := evalSimpleRecursive(b[:len(b)-1], t).Scale(1 - t)
+	term2 := evalSimpleRecursive(b[1:], t).Scale(t)
+	return term1.Add(term2)
+}
+
 func TestBezierInverseX(t *testing.T) {
 	curves := []BezierCurve{
 		{
