46156c03a730aaa66b35787cf2cb667873bc7235
Alex Nichol <unixpickle@gmail.com>
2020-08-01 14:14:21 -0400

    experiment with telescoping interlocking tubes

---
+++ examples/experiments/telescoping/README.md
@@ -0,0 +1,11 @@
+# telescoping
+
+This is an attempt to make a series of interlocking telescoping tubes in a single print. In particular, this demo creates a model consisting of three interlocking tubes, which can be extended outward, but cannot be separated. See [Whip antenna](https://en.wikipedia.org/wiki/Whip_antenna).
+
+To allow printing on an FDM printer, the notches in the tubes are triangular, extending at a 45 degree angle, to avoid the need for supports.
+
+# Rendering
+
+Here is a rendering of the first attempt:
+
+![Rendering of the model](rendering.png)
---
+++ examples/experiments/telescoping/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"log"
+	"math"
+
+	"github.com/unixpickle/model3d/render3d"
+	"github.com/unixpickle/model3d/toolbox3d"
+
+	"github.com/unixpickle/model3d/model3d"
+)
+
+const (
+	NotchSize     = 0.05
+	TubeThickness = 0.1
+
+	OuterRadius = 1.0
+	OuterHeight = 3.0
+
+	GapRadius = TubeThickness + NotchSize*1.5
+	GapHeight = NotchSize * 2
+
+	NumTubes = 3
+)
+
+func main() {
+	radius := OuterRadius
+	height := OuterHeight
+	tubes := model3d.JoinedSolid{
+		// Outer tube.
+		&Tube{
+			Radius:       radius,
+			Height:       height,
+			InnerNotches: []float64{height - NotchSize},
+		},
+	}
+
+	// Create tubes going inward.
+	for i := 0; i < NumTubes-1; i++ {
+		radius -= GapRadius
+		height += GapHeight
+		tubes = append(tubes, &Tube{
+			Radius:       radius,
+			Height:       height,
+			OuterNotches: []float64{0.0, height - NotchSize},
+			InnerNotches: []float64{height - NotchSize},
+		})
+	}
+
+	log.Println("Creating mesh...")
+	ax := &toolbox3d.AxisSqueeze{
+		Axis:  toolbox3d.AxisZ,
+		Min:   NotchSize*2 + 0.1,
+		Max:   OuterHeight - NotchSize*2 - 0.1,
+		Ratio: 0.1,
+	}
+	mesh := model3d.MarchingCubesSearch(model3d.TransformSolid(ax, tubes), 0.01, 16)
+	mesh = mesh.MapCoords(ax.Inverse().Apply)
+	log.Println("Simplifying mesh...")
+	mesh = mesh.EliminateCoplanar(1e-5)
+	log.Println("Saving mesh...")
+	mesh.SaveGroupedSTL("telescoping.stl")
+	log.Println("Rendering...")
+	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 300, nil)
+}
+
+type Tube struct {
+	Radius       float64
+	Height       float64
+	InnerNotches []float64
+	OuterNotches []float64
+}
+
+func (t *Tube) Min() model3d.Coord3D {
+	mx := -(t.Radius + TubeThickness/2 + NotchSize)
+	return model3d.XY(mx, mx)
+}
+
+func (t *Tube) Max() model3d.Coord3D {
+	mx := t.Radius + TubeThickness/2 + NotchSize
+	return model3d.XYZ(mx, mx, t.Height)
+}
+
+func (t *Tube) Contains(c model3d.Coord3D) bool {
+	if !model3d.InBounds(t, c) {
+		return false
+	}
+	cRadius := c.XY().Norm()
+
+	// Fast path for most of the volume.
+	if cRadius < (t.Radius - TubeThickness/2 - NotchSize) {
+		return false
+	} else if cRadius > (t.Radius + TubeThickness/2 + NotchSize) {
+		return false
+	}
+
+	minRadius := t.Radius - TubeThickness/2
+	maxRadius := t.Radius + TubeThickness/2
+
+	for _, notchZ := range t.InnerNotches {
+		extraRadius := NotchSize - math.Abs(c.Z-notchZ)
+		if extraRadius > 0 {
+			minRadius = math.Min(minRadius, t.Radius-TubeThickness/2-extraRadius)
+		}
+	}
+
+	for _, notchZ := range t.OuterNotches {
+		extraRadius := NotchSize - math.Abs(c.Z-notchZ)
+		if extraRadius > 0 {
+			maxRadius = math.Max(maxRadius, t.Radius+TubeThickness/2+extraRadius)
+		}
+	}
+
+	return cRadius >= minRadius && cRadius <= maxRadius
+}
