f540de183adc80338bef808dfbbacb1f16c09f6a
Alex Nichol <unixpickle@gmail.com>
2020-11-24 16:11:46 -0500

    add API to export multiple 2d meshes

--- model2d/export.go
+++ model2d/export.go
@@ -6,8 +6,34 @@ import (
 )
 
 func EncodeSVG(m *Mesh) []byte {
-	min := m.Min()
-	max := m.Max()
+	return EncodeCustomSVG([]*Mesh{m}, []string{"black"}, []float64{1.0}, nil)
+}
+
+// EncodeCustomSVG encodes multiple meshes, each with a
+// different color and line thickness.
+//
+// If bounds is not nil, it is used to determine the
+// resulting bounds of the SVG.
+// Otherwise, the union of all meshes is used.
+func EncodeCustomSVG(meshes []*Mesh, colors []string, thicknesses []float64, bounds Bounder) []byte {
+	if len(meshes) != len(colors) {
+		panic("incorrect number of colors")
+	}
+	if len(meshes) != len(thicknesses) {
+		panic("incorrect number of thicknesses")
+	}
+
+	var min, max Coord
+	if bounds != nil {
+		min, max = bounds.Min(), bounds.Max()
+	} else {
+		min = meshes[0].Min()
+		max = meshes[0].Max()
+		for _, m := range meshes {
+			min = m.Min().Min(min)
+			max = m.Max().Max(max)
+		}
+	}
 
 	var result strings.Builder
 	result.WriteString(`<?xml version="1.0" encoding="utf-8" ?>`)
@@ -15,20 +41,25 @@ func EncodeSVG(m *Mesh) []byte {
 		fmt.Sprintf(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="%f %f %f %f">`,
 			min.X, min.Y, max.X-min.X, max.Y-min.Y))
 
-	findPolylines(m, func(points []Coord) {
-		pointStrs := make([]string, len(points))
-		for i, c := range points {
-			pointStrs[i] = fmt.Sprintf("%f,%f", c.X, c.Y)
-		}
-		if pointStrs[0] == pointStrs[len(pointStrs)-1] {
-			pointStrs = pointStrs[1:]
-			result.WriteString(`<polygon points="`)
-		} else {
-			result.WriteString(`<polyline points="`)
-		}
-		result.WriteString(strings.Join(pointStrs, " "))
-		result.WriteString(`" stroke="black" fill="none" />`)
-	})
+	for i, m := range meshes {
+		color := colors[i]
+		thickness := fmt.Sprintf("%f", thicknesses[i])
+		findPolylines(m, func(points []Coord) {
+			pointStrs := make([]string, len(points))
+			for i, c := range points {
+				pointStrs[i] = fmt.Sprintf("%f,%f", c.X, c.Y)
+			}
+			if pointStrs[0] == pointStrs[len(pointStrs)-1] {
+				pointStrs = pointStrs[1:]
+				result.WriteString(`<polygon points="`)
+			} else {
+				result.WriteString(`<polyline points="`)
+			}
+			result.WriteString(strings.Join(pointStrs, " "))
+			result.WriteString(`" stroke="` + color + `" fill="none" `)
+			result.WriteString(`stroke-width="` + thickness + `" />`)
+		})
+	}
 
 	result.WriteString("</svg>")
 	return []byte(result.String())
