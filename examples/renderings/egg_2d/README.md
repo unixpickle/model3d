e2c044e5247ed27af884f42ecde661818c566b42
Alex Nichol <unixpickle@gmail.com>
2021-03-09 17:30:45 -0500

    add eggsample for colorized rasterization

---
+++ examples/renderings/egg_2d/README.md
@@ -0,0 +1,9 @@
+# egg_2d
+
+This example shows how to rasterize multiple 2D shapes on top of each other, in this case to make a cartoon sunny-side up egg.
+
+# Renderings
+
+Here is the output
+
+![Egg](egg.png)
---
+++ examples/renderings/egg_2d/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"image/color"
+	"math"
+
+	"github.com/unixpickle/model3d/model2d"
+)
+
+func main() {
+	// Create a wiggly egg.
+	egg := model2d.NewMeshPolar(func(theta float64) float64 {
+		return 0.9 + 0.1*math.Sin(theta*5)
+	}, 300)
+	circle := &model2d.Circle{Radius: 0.3}
+	objs := []interface{}{
+		model2d.NewColliderSolid(model2d.MeshToCollider(egg)),
+		circle,
+	}
+	model2d.RasterizeColor("egg.png", objs, []color.Color{
+		color.Gray{Y: 0xff},
+		color.RGBA{R: 0xff, G: 0xff, A: 0xff},
+	}, 200.0)
+}
