90638acc15b8bb4d57802915f8e689156e925512
Alex Nichol <unixpickle@gmail.com>
2021-03-20 12:22:06 -0400

    pitcher: handle, cleanup, README

---
+++ examples/usable/pitcher/README.md
@@ -0,0 +1,9 @@
+# pitcher
+
+This example creates a water pitcher with a handle and a lip. It is fully 3D-printable without supports, and is intended for watering plants!
+
+# Rendering
+
+Here is what the model looks like:
+
+![Rendering of the pitcher](rendering.png)
--- examples/usable/pitcher/main.go
+++ examples/usable/pitcher/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"math"
 
 	"github.com/unixpickle/model3d/model3d"
@@ -13,12 +14,30 @@ const (
 	PitcherHeight = 5.0
 	LipHeight     = 1.0
 	LipRadius     = 0.5
+	LipInset      = 0.1
+	HandleHeight  = 2.5
+	HandleWidth   = 0.3
 	Thickness     = 0.2
 )
 
 func main() {
+	log.Println("Creating solid...")
 	model := PitcherExteriorSolid()
-	mesh := model3d.MarchingCubesSearch(model, 0.02, 8)
+
+	log.Println("Creating mesh...")
+	squeeze := toolbox3d.NewSmartSqueeze(toolbox3d.AxisZ, 0, 0.04, 0)
+	squeeze.AddUnsqueezable(PitcherHeight-LipHeight-0.1, math.Inf(1))
+	squeeze.AddUnsqueezable(PitcherHeight/2-HandleHeight/2-0.1, PitcherHeight/2+HandleHeight/2+0.1)
+	squeeze.AddPinch(-Thickness / 2)
+	mesh := squeeze.MarchingCubesSearch(model, 0.02, 8)
+
+	log.Println("Simplifying mesh...")
+	mesh = mesh.EliminateCoplanar(1e-5)
+
+	log.Println("Saving mesh...")
+	mesh.SaveGroupedSTL("pitcher.stl")
+
+	log.Println("Rendering...")
 	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 300, nil)
 }
 
@@ -26,7 +45,7 @@ func PitcherExteriorSolid() model3d.Solid {
 	model := PitcherExterior()
 	squeeze := &toolbox3d.AxisSqueeze{
 		Axis:  toolbox3d.AxisZ,
-		Min:   0,
+		Min:   0.1,
 		Max:   PitcherHeight - LipHeight - 0.1,
 		Ratio: 0.1,
 	}
@@ -41,7 +60,18 @@ func PitcherExteriorSolid() model3d.Solid {
 			mesh.Remove(t)
 		}
 	})
-	return model3d.NewColliderSolidHollow(model3d.MeshToCollider(mesh), Thickness/2)
+	rounded := model3d.NewColliderSolidHollow(model3d.MeshToCollider(mesh), Thickness/2)
+	return model3d.JoinedSolid{
+		rounded,
+		PitcherHandle(),
+		// Solid cylinder at the base instead of a
+		// rounded base (which needs support).
+		&model3d.Cylinder{
+			P1:     model3d.Z(-Thickness / 2),
+			P2:     model3d.Z(Thickness / 2),
+			Radius: PitcherRadius + Thickness/2,
+		},
+	}
 }
 
 func PitcherExterior() model3d.Solid {
@@ -49,27 +79,46 @@ func PitcherExterior() model3d.Solid {
 		Radius: PitcherRadius,
 		P2:     model3d.Z(PitcherHeight),
 	}
-	parab := Parabaloid()
-	parab = model3d.TransformSolid(
-		&model3d.Matrix3Transform{Matrix: &model3d.Matrix3{LipRadius, 0, 0, 0, LipRadius, 0, 0, 0, LipHeight}},
-		parab,
+	lip := Pyramid()
+	lip = model3d.TransformSolid(
+		&model3d.Matrix3Transform{
+			Matrix: &model3d.Matrix3{
+				LipRadius, 0, 0, 0, LipRadius, 0, 0, 0, LipHeight,
+			},
+		},
+		lip,
 	)
-	parab = model3d.TransformSolid(
-		&model3d.Translate{Offset: model3d.XZ(PitcherRadius, PitcherHeight-parab.Max().Z)},
-		parab,
+	lip = model3d.TransformSolid(
+		&model3d.Translate{Offset: model3d.XZ(-PitcherRadius+LipInset, PitcherHeight-lip.Max().Z)},
+		lip,
 	)
 	return model3d.JoinedSolid{
 		base,
-		parab,
+		lip,
 	}
 }
 
-func Parabaloid() model3d.Solid {
+func Pyramid() model3d.Solid {
 	return model3d.CheckedFuncSolid(
 		model3d.XYZ(-1, -1, 0),
 		model3d.XYZ(1, 1, 1),
 		func(c model3d.Coord3D) bool {
-			return c.Z > c.X*c.X+c.Y*c.Y
+			return c.Z > math.Abs(c.X)+math.Abs(c.Y)
+		},
+	)
+}
+
+func PitcherHandle() model3d.Solid {
+	x := PitcherRadius
+	z := PitcherHeight / 2
+	inset := Thickness * math.Sqrt(2)
+	return model3d.CheckedFuncSolid(
+		model3d.XYZ(x-Thickness/2, -HandleWidth/2, z-HandleHeight/2),
+		model3d.XYZ(x+HandleHeight/2, HandleWidth/2, z+HandleHeight/2),
+		func(c model3d.Coord3D) bool {
+			zDist := math.Abs(c.Z - z)
+			curX := x + HandleHeight/2 - zDist
+			return c.X < curX && c.X > curX-inset
 		},
 	)
 }
