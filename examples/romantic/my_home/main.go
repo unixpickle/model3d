3f23a2871ed40091cac76a2e07f9f69fe80d23c9
Alex Nichol <unixpickle@gmail.com>
2021-03-24 17:44:44 -0400

    use toolbox3d in my_home example

--- examples/romantic/my_home/main.go
+++ examples/romantic/my_home/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/unixpickle/model3d/model2d"
 	"github.com/unixpickle/model3d/model3d"
 	"github.com/unixpickle/model3d/render3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 )
 
 func main() {
@@ -87,80 +88,25 @@ func WindowSolid() model3d.Solid {
 	const thickness = 0.015
 
 	return model3d.JoinedSolid{
-		TriangularPolygon(
+		toolbox3d.TriangularPolygon(
 			thickness, true,
 			model3d.XZ(-size, -size),
 			model3d.XZ(-size, size),
 			model3d.XZ(size, size),
 			model3d.XZ(size, -size),
 		),
-		TriangularLine(thickness, model3d.X(-size+thickness/2), model3d.X(size-thickness/2)),
-		TriangularLine(thickness, model3d.Z(-size+thickness/2), model3d.Z(size-thickness/2)),
+		toolbox3d.TriangularLine(thickness, model3d.X(-size+thickness/2), model3d.X(size-thickness/2)),
+		toolbox3d.TriangularLine(thickness, model3d.Z(-size+thickness/2), model3d.Z(size-thickness/2)),
 	}
 }
 
 func DoorSolid() model3d.Solid {
 	const size = 0.15
 	return model3d.JoinedSolid{
-		TriangularPolygon(
+		toolbox3d.TriangularPolygon(
 			0.02, false, model3d.XYZ(-size, 0.5, 0), model3d.XYZ(-size, 0.5, 0.45),
 			model3d.XYZ(size, 0.5, 0.45), model3d.XYZ(size, 0.5, 0),
 		),
-		TriangularBall(0.03, model3d.XYZ(-size+0.07, 0.5, 0.45/2)),
+		toolbox3d.TriangularBall(0.03, model3d.XYZ(-size+0.07, 0.5, 0.45/2)),
 	}
-}
-
-func TriangularPolygon(thickness float64, close bool, p ...model3d.Coord3D) model3d.Solid {
-	res := model3d.JoinedSolid{}
-	for i := 0; i < len(p)-1; i++ {
-		res = append(res, TriangularLine(thickness, p[i], p[i+1]))
-		if i != 0 {
-			res = append(res, TriangularBall(thickness, p[i]))
-		}
-	}
-	if close {
-		res = append(
-			res,
-			TriangularLine(thickness, p[len(p)-1], p[0]),
-			TriangularBall(thickness, p[len(p)-1]),
-			TriangularBall(thickness, p[0]),
-		)
-	}
-	return res.Optimize()
-}
-
-func TriangularLine(thickness float64, p1, p2 model3d.Coord3D) model3d.Solid {
-	dir := p1.Sub(p2)
-	length := dir.Norm()
-	dir = dir.Normalize()
-
-	// Basis vectors will be axis-aligned if dir is.
-	b1, b2 := dir.OrthoBasis()
-
-	ball := model3d.XYZ(thickness, thickness, thickness)
-	return model3d.CheckedFuncSolid(
-		p1.Min(p2).Sub(ball),
-		p1.Max(p2).Add(ball),
-		func(c model3d.Coord3D) bool {
-			subtracted := c.Sub(p2)
-			dot := subtracted.Dot(dir)
-			if dot < 0 || dot > length {
-				return false
-			}
-			dot1, dot2 := b1.Dot(subtracted), b2.Dot(subtracted)
-			return math.Abs(dot1)+math.Abs(dot2) < thickness
-		},
-	)
-}
-
-func TriangularBall(thickness float64, p model3d.Coord3D) model3d.Solid {
-	ball := model3d.XYZ(thickness, thickness, thickness)
-	return model3d.CheckedFuncSolid(
-		p.Sub(ball),
-		p.Add(ball),
-		func(c model3d.Coord3D) bool {
-			diff := c.Sub(p)
-			return math.Abs(diff.X)+math.Abs(diff.Y)+math.Abs(diff.Z) < thickness
-		},
-	)
 }
