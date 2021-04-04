cb874028817dd291dd944648124964bc0be4352d
Alex Nichol <unixpickle@gmail.com>
2021-04-04 08:43:15 -0400

    test LineSearch on polynomials

---
+++ toolbox3d/min_max_test.go
@@ -0,0 +1,60 @@
+package toolbox3d
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func TestLineSearch(t *testing.T) {
+	polynomials := [][3]float64{
+		{1, 2, -3},
+		{2, 3, 5},
+		{-1, 2, 3},
+	}
+
+	polyMinMax := func(maximize bool, poly [3]float64) (float64, float64) {
+		extremum := -poly[1] / (2 * poly[0])
+
+		bestVal := math.Inf(1)
+		if maximize {
+			bestVal = -bestVal
+		}
+		bestX := 0.0
+
+		for _, x := range []float64{-10, extremum, 10} {
+			if x < -10 || x > 10 {
+				// Avoid extremum outside of bounds.
+				continue
+			}
+			y := poly[0]*x*x + poly[1]*x + poly[2]
+			if (maximize && y > bestVal) || (!maximize && y < bestVal) {
+				bestVal = y
+				bestX = x
+			}
+		}
+
+		return bestX, bestVal
+	}
+
+	search := LineSearch{Stops: 10, Recursions: 10}
+	for _, poly := range polynomials {
+		t.Run(fmt.Sprintf("%f-%f-%f", poly[0], poly[1], poly[2]), func(t *testing.T) {
+			polyFunc := func(x float64) float64 {
+				return poly[0]*x*x + poly[1]*x + poly[2]
+			}
+			actualX, actualY := search.Minimize(-10, 10, polyFunc)
+			expectedX, expectedY := polyMinMax(false, poly)
+			if math.Abs(actualX-expectedX) > 1e-5 || math.Abs(actualY-expectedY) > 1e-5 {
+				t.Errorf("poly %v should have minimum (%f,%f) but got (%f,%f)",
+					poly, expectedX, expectedY, actualX, actualY)
+			}
+			actualX, actualY = search.Maximize(-10, 10, polyFunc)
+			expectedX, expectedY = polyMinMax(true, poly)
+			if math.Abs(actualX-expectedX) > 1e-5 || math.Abs(actualY-expectedY) > 1e-5 {
+				t.Errorf("poly %v should have maximum (%f,%f) but got (%f,%f)",
+					poly, expectedX, expectedY, actualX, actualY)
+			}
+		})
+	}
+}
