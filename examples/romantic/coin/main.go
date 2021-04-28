dcff22ce9c3cc36c89042d0cf4687ee06134ec9d
Alex Nichol <unixpickle@gmail.com>
2021-04-28 16:28:57 -0400

    option to create rounded coins

--- examples/romantic/coin/main.go
+++ examples/romantic/coin/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/unixpickle/model3d/model2d"
 	"github.com/unixpickle/model3d/model3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 
 	"github.com/unixpickle/essentials"
 	"github.com/unixpickle/model3d/render3d"
@@ -21,9 +22,12 @@ func main() {
 	var minHeight float64
 	var maxHeight float64
 	var msResolution float64
+	var mcDelta float64
 	var smoothIters int
 	var radius float64
 	var template string
+	var rounded bool
+	var roundSamples int
 
 	flag.StringVar(&outFile, "out", "coin.stl", "output file name")
 	flag.StringVar(&renderFile, "render", "rendering.png", "rendered output file name")
@@ -31,16 +35,34 @@ func main() {
 	flag.Float64Var(&maxHeight, "max-height", 0.13, "maximum height")
 	flag.Float64Var(&msResolution, "resolution", 0.01,
 		"resolution of marching squares, relative to radius")
+	flag.Float64Var(&mcDelta, "mc-delta", 0.01,
+		"resolution of marching cubes when creating a rounded solid")
 	flag.IntVar(&smoothIters, "smooth-iters", 50,
 		"number of mesh smoothing iterations")
 	flag.Float64Var(&radius, "radius", 0.5, "radius of coin")
 	flag.StringVar(&template, "template", "example.png", "coin design image")
+	flag.BoolVar(&rounded, "rounded", false, "use a rounded design instead of flat")
+	flag.IntVar(&roundSamples, "round-samples", 10000,
+		"number of samples to use for rounded design")
 
 	flag.Parse()
 
 	log.Println("Creating 2D mesh from template...")
 	mesh := ReadTemplateIntoMesh(template, msResolution, smoothIters, radius)
 
+	var mesh3d *model3d.Mesh
+	if rounded {
+		mesh3d = RoundedModel(mesh, minHeight, maxHeight, mcDelta, roundSamples)
+	} else {
+		mesh3d = UnroundedModel(mesh, minHeight, maxHeight)
+	}
+
+	log.Println("Saving...")
+	essentials.Must(mesh3d.SaveGroupedSTL(outFile))
+	essentials.Must(render3d.SaveRandomGrid(renderFile, mesh3d, 4, 4, 200, nil))
+}
+
+func UnroundedModel(mesh *model2d.Mesh, minHeight, maxHeight float64) *model3d.Mesh {
 	mesh3d := model3d.NewMesh()
 
 	// Create a separate 3D entity for each outer mesh.
@@ -94,9 +116,44 @@ func main() {
 		}
 	})
 
-	log.Println("Saving...")
-	essentials.Must(mesh3d.SaveGroupedSTL(outFile))
-	essentials.Must(render3d.SaveRandomGrid(renderFile, mesh3d, 4, 4, 200, nil))
+	return mesh3d
+}
+
+func RoundedModel(mesh *model2d.Mesh, minHeight, maxHeight, mcDelta float64,
+	roundSamples int) *model3d.Mesh {
+	// Create the outline for the base.
+	log.Println("Creating outline solid...")
+	outerMeshes := model2d.MeshToHierarchy(mesh)
+	outerSolid := model2d.JoinedSolid{}
+	for _, m := range outerMeshes {
+		outerSolid = append(outerSolid, model2d.NewColliderSolid(model2d.MeshToCollider(m.Mesh)))
+	}
+
+	// Create heightmap for the etching on the top.
+	log.Println("Creating heightmap...")
+	sdf2d := model2d.MeshToSDF(mesh)
+	hm := toolbox3d.NewHeightMap(sdf2d.Min(), sdf2d.Max(), 1024)
+	for i := 0; i < roundSamples; i++ {
+		p := model2d.NewCoordRandBounds(hm.Min, hm.Max)
+		if sdf2d.SDF(p) < 0 {
+			i--
+			continue
+		}
+		proj := model2d.ProjectMedialAxis(sdf2d, p, 16, 0)
+		hm.AddSphereFill(proj, sdf2d.SDF(proj), maxHeight-minHeight)
+	}
+
+	log.Println("Creating mesh...")
+	solid := model3d.JoinedSolid{
+		model3d.ProfileSolid(outerSolid, -minHeight, 1e-5),
+		toolbox3d.HeightMapToSolid(hm),
+	}
+	mesh3d := model3d.MarchingCubesSearch(solid, mcDelta, 8)
+
+	log.Println("Simplifying mesh...")
+	mesh3d = mesh3d.EliminateCoplanar(1e-5)
+
+	return mesh3d
 }
 
 func ReadTemplateIntoMesh(filename string, msResolution float64, smoothIters int,
