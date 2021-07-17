13bdae960eede7ef440bfa0a0402967fbcd235e1
Alex Nichol <unixpickle@gmail.com>
2021-07-17 15:24:36 -0400

    polynomial + root finding

---
+++ numerical/polynomial.go
@@ -0,0 +1,168 @@
+package numerical
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+)
+
+// A Polynomial is an equation of the form
+//
+//     a0 + a1*x + a2*x^2 + a3*x^3 + ...
+//
+// Here, the polynomial is represented as an array of
+// [a0, a1, ...].
+type Polynomial []float64
+
+// String returns a string representation of p.
+func (p Polynomial) String() string {
+	parts := make([]string, len(p))
+	for i, x := range p {
+		parts[i] = fmt.Sprintf("%f", x)
+		if i == 1 {
+			parts[i] += "x"
+		} else if i > 1 {
+			parts[i] += fmt.Sprintf("x^%d", i)
+		}
+	}
+	return strings.Join(parts, " + ")
+}
+
+// Eval evaluates the polynomial at the given x value.
+func (p Polynomial) Eval(x float64) float64 {
+	xP := 1.0
+	res := 0.0
+	for _, c := range p {
+		res += c * xP
+		xP *= x
+	}
+	return res
+}
+
+// Derivative computes the derivative of the polynomial.
+func (p Polynomial) Derivative() Polynomial {
+	if len(p) == 1 {
+		return Polynomial{}
+	}
+	res := make(Polynomial, len(p)-1)
+	for i, c := range p[1:] {
+		res[i] = c * float64(i+1)
+	}
+	return res
+}
+
+// RealRoots computes the real roots of the p, i.e. values
+// of x such that p(x) = 0. The result may have duplicates
+// since roots can be repeated.
+//
+// If the polynomial has an infinite number of roots, one
+// NaN root is returned.
+func (p Polynomial) RealRoots() []float64 {
+	if len(p) == 0 {
+		return []float64{math.NaN()}
+	} else if p[len(p)-1] == 0 {
+		return p[:len(p)-1].RealRoots()
+	} else if len(p) == 1 {
+		return []float64{}
+	} else if len(p) == 2 {
+		// Inverse of linear equation.
+		return []float64{-p[0] / p[1]}
+	} else if len(p) == 3 {
+		// Quadratic formula.
+		a, b, c := p[2], p[1], p[0]
+		sqrtMe := b*b - 4*a*c
+		if sqrtMe < 0 {
+			return []float64{}
+		}
+		sqrt := math.Sqrt(sqrtMe)
+		root1 := (-b + sqrt) / (2 * a)
+		root2 := (-b - sqrt) / (2 * a)
+		return []float64{root1, root2}
+	}
+
+	// Cauchy's bound for real roots.
+	absBound := 0.0
+	for _, x := range p[:len(p)-1] {
+		absBound = math.Max(absBound, math.Abs(x/p[len(p)-1]))
+	}
+	absBound += 1.0
+
+	y1 := p.Eval(-absBound)
+	y2 := p.Eval(absBound)
+
+	// Fast path to avoid finding roots of derivative.
+	if y1 == 0 {
+		return append(p.divideRoot(-absBound).RealRoots(), -absBound)
+	} else if y2 == 0 {
+		return append(p.divideRoot(absBound).RealRoots(), absBound)
+	} else if (y1 < 0) != (y2 < 0) {
+		r := p.searchRoot(-absBound, absBound)
+		return append(p.divideRoot(r).RealRoots(), r)
+	}
+
+	extrema := append(append([]float64{-absBound}, p.Derivative().RealRoots()...), absBound)
+	sort.Float64s(extrema)
+
+	for i, x1 := range extrema[:len(extrema)-1] {
+		x2 := extrema[i+1]
+		y1 := p.Eval(x1)
+		y2 := p.Eval(x2)
+		if y1 == 0 {
+			return append(p.divideRoot(x1).RealRoots(), x1)
+		} else if y2 == 0 {
+			return append(p.divideRoot(x2).RealRoots(), x2)
+		} else if (y1 < 0) != (y2 < 0) {
+			r := p.searchRoot(x1, x2)
+			return append(p.divideRoot(r).RealRoots(), r)
+		}
+	}
+
+	return []float64{}
+}
+
+// divideRoot returns the polynomial resulting from
+// dividing the root x out of p.
+// This will yield invalid results if x is not a root.
+func (p Polynomial) divideRoot(x float64) Polynomial {
+	if len(p) < 2 {
+		panic("cannot divide root out of constant equation")
+	} else if len(p) == 2 {
+		// Assume that the root is correct.
+		return Polynomial{1}
+	}
+	res := make(Polynomial, len(p)-1)
+	temp := append(Polynomial{}, p...)
+	for i := len(p) - 1; i > 0; i-- {
+		res[i-1] = temp[i]
+		temp[i-1] += x * temp[i]
+	}
+	return res
+}
+
+// searchRoot finds a root between to x values with
+// different signs for p(x).
+func (p Polynomial) searchRoot(x1, x2 float64) float64 {
+	y1 := p.Eval(x1)
+	diff := math.Abs(x1 - x2)
+	for {
+		mx := (x1 + x2) / 2
+		my := p.Eval(mx)
+		if my == 0 {
+			return mx
+		} else if (my < 0) == (y1 < 0) {
+			x1 = mx
+			y1 = my
+		} else {
+			x2 = mx
+		}
+		diff1 := math.Abs(x1 - x2)
+		if diff1 >= diff {
+			// Our bounds are as close as they can be in
+			// this numeric format.
+			break
+		}
+		diff = diff1
+	}
+	return (x1 + x2) / 2
+}
---
+++ numerical/polynomial_test.go
@@ -0,0 +1,98 @@
+package numerical
+
+import (
+	"math"
+	"testing"
+
+	"github.com/unixpickle/essentials"
+)
+
+func TestPolynomialDivideRoot(t *testing.T) {
+	p1 := Polynomial{-10, -3, 1}
+	p2 := p1.divideRoot(-2)
+	if len(p2) != 2 || math.Abs(p2[0]+5) > 1e-5 || math.Abs(p2[1]-1) > 1e-5 {
+		t.Errorf("unexpected quotient: %v", p2)
+	} else if math.IsNaN(p2[0]) || math.IsNaN(p2[1]) || math.IsInf(p2[0], 0) || math.IsInf(p2[1], 0) {
+		t.Errorf("invalid numeric in result: %v", p2)
+	}
+
+	// (2x^2+3x+4)(x-5) = 2x^3-7x^2-11x-20
+	p1 = Polynomial{-20, -11, -7, 2}
+	actual := p1.divideRoot(5)
+	expected := Polynomial{4, 3, 2}
+	if len(actual) != len(expected) {
+		t.Errorf("bad length in result: %v", actual)
+	} else {
+		for i, x := range expected {
+			a := actual[i]
+			if math.IsNaN(a) || math.IsInf(a, 0) {
+				t.Errorf("invalid numeric in result: %v", actual)
+				break
+			} else if math.Abs(a-x) > 1e-5 {
+				t.Errorf("unexpected quotient: %v", actual)
+			}
+		}
+	}
+}
+
+func TestPolynomialSearchRoot(t *testing.T) {
+	// (2x^2+3x+4)(x-5) = 2x^3-7x^2-11x-20
+	p := Polynomial{-20, -11, -7, 2}
+	root := p.searchRoot(4.9, 5.1)
+	if math.IsNaN(root) || math.IsInf(root, 0) || math.Abs(root-5) > 1e-5 {
+		t.Errorf("unexpected root: %v", root)
+	}
+}
+
+func TestPolynomialRealRoots(t *testing.T) {
+	checkRoots := func(t *testing.T, actual []float64, expected []float64) {
+		if len(actual) != len(expected) {
+			t.Fatalf("bad roots: %v (expected %v)", actual, expected)
+		}
+		leftover := append([]float64{}, expected...)
+		for _, a := range actual {
+			if math.IsNaN(a) || math.IsInf(a, 0) {
+				t.Fatalf("bad roots: %v (expected %v)", actual, expected)
+			}
+			found := false
+			for i, x := range leftover {
+				if math.Abs(a-x) < 1e-5 {
+					found = true
+					essentials.UnorderedDelete(&leftover, i)
+					break
+				}
+			}
+			if !found {
+				t.Fatalf("bad roots: %v (expected %v)", actual, expected)
+			}
+		}
+	}
+
+	t.Run("Quad", func(t *testing.T) {
+		// 2(x-2)(x-3) = 2x^2-10x+12
+		p := Polynomial{12, -10, 2}
+		roots := p.RealRoots()
+		checkRoots(t, roots, []float64{2, 3})
+	})
+
+	t.Run("CubicSingle", func(t *testing.T) {
+		// 2x^3-7x^2-11x-20 has only one real root, 5.
+		p := Polynomial{-20, -11, -7, 2}
+		roots := p.RealRoots()
+		checkRoots(t, roots, []float64{5})
+	})
+
+	t.Run("Quartic", func(t *testing.T) {
+		// (x+2)(x-3)(x^2+2x+7) = x^4+x^3-x^2-19x-42
+		p := Polynomial{-42, -19, -1, 1, 1}
+		roots := p.RealRoots()
+		checkRoots(t, roots, []float64{-2, 3})
+	})
+
+	t.Run("Large", func(t *testing.T) {
+		// (x+5)(x-7)(x+3)(x+2)(x-3)(x^2+2x+7)^2
+		p := Polynomial{30870, 34839, 17738, 4526, -570, -744, -234, -30, 4, 1}
+		roots := p.RealRoots()
+		checkRoots(t, roots, []float64{-5, 7, -3, -2, 3})
+	})
+}
