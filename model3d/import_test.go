b3c1693964bf0320aae1dbeb0c24a5c7cee9cc76
Alex Nichol <unixpickle@gmail.com>
2021-07-17 09:28:09 -0400

    check volume+area in ImportOFF test

--- model3d/import_test.go
+++ model3d/import_test.go
@@ -48,4 +48,13 @@ func TestImportOFF(t *testing.T) {
 	if len(triangles) != 12 {
 		t.Errorf("expected %d triangles but got %d", 12, len(triangles))
 	}
+	mesh := NewMeshTriangles(triangles)
+	volume := mesh.Volume()
+	if math.Abs(volume-1) > 1e-5 || math.IsNaN(volume) || math.IsInf(volume, 0) {
+		t.Errorf("incorrect volume: %f", volume)
+	}
+	area := mesh.Area()
+	if math.Abs(area-6) > 1e-5 || math.IsNaN(area) || math.IsInf(area, 0) {
+		t.Errorf("incorrect area: %f", area)
+	}
 }
