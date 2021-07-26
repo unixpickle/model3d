baae38c70a2d1ad1602c2563a31fde2dfa396d68
Alex Nichol <unixpickle@gmail.com>
2021-07-26 11:00:38 -0400

    script for fitting bezier curves & rendering

---
+++ examples/experiments/smooth_bezier/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"log"
+	"os"
+
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/model3d/model2d"
+)
+
+func main() {
+	var smoothIters int
+	var tolerance float64
+	var upsampleRate float64
+	var thicken float64
+	var outline bool
+	var animationPath string
+	flag.IntVar(&smoothIters, "smooth-iters", 100, "number of smoothing iterations")
+	flag.Float64Var(&tolerance, "tolerance", model2d.DefaultBezierFitTolerance, "MSE tolerance")
+	flag.Float64Var(&upsampleRate, "upsample-rate", 2.0, "extra resolution to add to output")
+	flag.Float64Var(&thicken, "thicken", 0.0, "extra thickness (in pixels) to give to the output")
+	flag.BoolVar(&outline, "outline", false, "produce an outline instead of a solid")
+	flag.StringVar(&animationPath, "animation", "", "path for output GIF")
+
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "[flags] <input> <output>")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "Flags:")
+		fmt.Fprintln(os.Stderr)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		flag.Usage()
+	}
+
+	log.Println("Reading file as mesh...")
+	mesh, size := ReadImage(flag.Args()[0])
+
+	log.Println("Smoothing mesh...")
+	mesh = mesh.SmoothSq(smoothIters)
+
+	log.Println("Fitting beziers...")
+	fitter := &model2d.BezierFitter{Tolerance: tolerance, Momentum: 0.5}
+	beziers := fitter.Fit(mesh)
+	log.Printf("Fit %d beziers.", len(beziers))
+
+	if animationPath != "" {
+		log.Println("Rendering animation sequence...")
+		RenderSequence(animationPath, upsampleRate, size, mesh, beziers)
+	}
+
+	mesh = model2d.NewMesh()
+	for _, b := range beziers {
+		n := 100
+		for i := 0; i < n; i++ {
+			t1 := float64(i) / float64(n)
+			t2 := float64(i+1) / float64(n)
+			mesh.Add(&model2d.Segment{
+				b.Eval(t1),
+				b.Eval(t2),
+			})
+		}
+	}
+
+	log.Println("Rendering...")
+	collider := model2d.MeshToCollider(mesh)
+	rast := &model2d.Rasterizer{Scale: upsampleRate, Bounds: model2d.NewRect(model2d.Coord{}, size)}
+	var img image.Image
+	if outline {
+		img = rast.RasterizeCollider(collider)
+	} else if thicken == 0 {
+		img = rast.RasterizeColliderSolid(collider)
+	} else {
+		img = rast.RasterizeSolid(model2d.NewColliderSolidInset(collider, -thicken))
+	}
+	essentials.Must(model2d.SaveImage(flag.Args()[1], img))
+}
+
+func RenderSequence(animationPath string, upsampleRate float64, size model2d.Coord,
+	mesh *model2d.Mesh, beziers []model2d.BezierCurve) {
+	objects := []interface{}{
+		model2d.NewRect(model2d.Coord{}, size),
+		mesh,
+	}
+	colors := []color.Color{
+		color.Gray{Y: 0xff},
+		color.Gray{Y: 0xd0},
+	}
+	thicknesses := []float64{1.0, 2.0}
+
+	palette := make(color.Palette, 0, 256)
+	for i := 0; i < 256/2; i++ {
+		frac := float64(i) / float64(256/2-1)
+		palette = append(palette, color.Gray{Y: uint8(frac * 0xff)})
+		palette = append(palette, color.RGBA{
+			R: uint8(frac * 0x65),
+			G: uint8(frac * 0xbc),
+			B: uint8(frac * 0xd4),
+			A: uint8(frac * 0xff),
+		})
+	}
+
+	g := &gif.GIF{}
+	rasterize := func(objects []interface{}, colors []color.Color, thicknesses []float64) {
+		imgs := make([]*image.Gray, len(objects))
+		for i, x := range objects {
+			rast := &model2d.Rasterizer{
+				Scale:     upsampleRate,
+				Bounds:    model2d.NewRect(model2d.Coord{}, size),
+				LineWidth: thicknesses[i],
+			}
+			imgs[i] = rast.Rasterize(x)
+		}
+		combined := model2d.ColorizeOverlay(imgs, colors)
+		frame := image.NewPaletted(combined.Bounds(), palette)
+		for y := 0; y < combined.Bounds().Dy(); y++ {
+			for x := 0; x < combined.Bounds().Dx(); x++ {
+				frame.Set(x, y, combined.At(x, y))
+			}
+		}
+		g.Image = append(g.Image, frame)
+		g.Delay = append(g.Delay, 30)
+	}
+
+	rasterize(objects, colors, thicknesses)
+	for _, b := range beziers {
+		oneBezier := model2d.NewMesh()
+		for j := 0; j < 100; j++ {
+			t1 := float64(j) / 100
+			t2 := float64(j+1) / 100
+			oneBezier.Add(&model2d.Segment{b.Eval(t1), b.Eval(t2)})
+		}
+		o1 := append(objects, oneBezier)
+		c1 := append(colors, color.RGBA{R: 0x65, G: 0xbc, B: 0xd4, A: 0xff})
+		t1 := append(thicknesses, 15.0)
+		rasterize(o1, c1, t1)
+		objects = append(objects, oneBezier)
+		colors = append(colors, color.Gray{Y: 0})
+		thicknesses = append(thicknesses, 7.0)
+		rasterize(objects, colors, thicknesses)
+	}
+
+	w, err := os.Create(animationPath)
+	essentials.Must(err)
+	defer w.Close()
+	gif.EncodeAll(w, g)
+}
+
+func ReadImage(path string) (mesh *model2d.Mesh, size model2d.Coord) {
+	bmp := model2d.MustReadBitmap(path, nil)
+	size = model2d.XY(float64(bmp.Width), float64(bmp.Height))
+	return bmp.Mesh(), size
+}
