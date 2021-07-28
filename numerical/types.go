efe315fbbde55fba61433c04973014d108d55531
Alex Nichol <unixpickle@gmail.com>
2021-07-28 08:42:58 -0400

    start working on arbitrary-length Vec

--- numerical/types.go
+++ numerical/types.go
@@ -44,3 +44,79 @@ func (v Vec3) Dist(v1 Vec3) float64 {
 func (v Vec3) Sum() float64 {
 	return v[0] + v[1] + v[2]
 }
+
+// Vec is a vector of arbitrary dimension.
+type Vec []float64
+
+// Add returns v + v1.
+func (v Vec) Add(v1 Vec) Vec {
+	if len(v) != len(v1) {
+		panic("vector lengths do not match")
+	}
+	res := make(Vec, len(v))
+	for i, x := range v {
+		y := v1[i]
+		res[i] = x + y
+	}
+	return res
+}
+
+// Sub returns v - v1.
+func (v Vec) Sub(v1 Vec) Vec {
+	if len(v) != len(v1) {
+		panic("vector lengths do not match")
+	}
+	res := make(Vec, len(v))
+	for i, x := range v {
+		y := v1[i]
+		res[i] = x - y
+	}
+	return res
+}
+
+// Dot returns the dot product of v and v1.
+func (v Vec) Dot(v1 Vec) float64 {
+	if len(v) != len(v1) {
+		panic("vector lengths do not match")
+	}
+	var res float64
+	for i, x := range v {
+		y := v1[i]
+		res += x * y
+	}
+	return res
+}
+
+// Scale returns v * s.
+func (v Vec) Scale(s float64) Vec {
+	res := make(Vec, len(v))
+	for i, x := range v {
+		res[i] = x * s
+	}
+	return res
+}
+
+// NormSquared returns ||v||^2.
+func (v Vec) NormSquared() float64 {
+	var res float64
+	for _, x := range v {
+		res += x * x
+	}
+	return res
+}
+
+// Norm returns ||v||.
+func (v Vec) Norm() float64 {
+	return math.Sqrt(v.NormSquared())
+}
+
+// Normalize returns v/||v||.
+func (v Vec) Normalize() Vec {
+	return v.Scale(1 / v.Norm())
+}
+
+// ProjectOut projects the direction v1 out of v.
+func (v Vec) ProjectOut(v1 Vec) Vec {
+	normed := v1.Normalize()
+	return v.Sub(normed.Scale(normed.Dot(v)))
+}
