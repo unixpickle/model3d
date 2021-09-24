efe391aa6714a269aac3f09de525406a90fa51bc
Alex Nichol <unixpickle@gmail.com>
2021-09-24 17:57:07 -0400

    render_stl: flags to export animated GIF

--- cli/render_stl/main.go
+++ cli/render_stl/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/unixpickle/essentials"
 	"github.com/unixpickle/model3d/model3d"
@@ -16,12 +17,16 @@ import (
 func main() {
 	var gridSize int
 	var imageSize int
+	var fps float64
+	var frames int
 	var verbose bool
 	flag.IntVar(&gridSize, "grid-size", 3, "grid size (used for rows and columns)")
 	flag.IntVar(&imageSize, "image-size", 300, "size of each image in the grid")
+	flag.Float64Var(&fps, "fps", 10.0, "FPS for GIF outputs")
+	flag.IntVar(&frames, "frames", 20, "total number of frames for GIF outputs")
 	flag.BoolVar(&verbose, "verbose", false, "run in verbose mode")
 	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, "Usage: "+os.Args[0]+" [flags] <model.stl> <output.png>")
+		fmt.Fprintln(os.Stderr, "Usage: "+os.Args[0]+" [flags] <model.stl> [output.png | output.gif]")
 		fmt.Fprintln(os.Stderr)
 		flag.PrintDefaults()
 	}
@@ -52,5 +57,18 @@ func main() {
 	if verbose {
 		log.Println("Rendering mesh to", outPath, "...")
 	}
-	essentials.Must(render3d.SaveRandomGrid(outPath, collider, gridSize, gridSize, imageSize, nil))
+	if strings.HasSuffix(outPath, ".gif") {
+		essentials.Must(render3d.SaveRotatingGIF(
+			outPath,
+			collider,
+			model3d.Z(1),
+			model3d.YZ(-1, 0.1).Normalize(),
+			imageSize,
+			frames,
+			fps,
+			nil,
+		))
+	} else {
+		essentials.Must(render3d.SaveRandomGrid(outPath, collider, gridSize, gridSize, imageSize, nil))
+	}
 }
