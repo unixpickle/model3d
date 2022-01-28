bc2d785bcf36589f0a4db14cba9df46a11c152f2
Alex Nichol <unixpickle@gmail.com>
2022-01-27 18:16:21 -0800

    layout arrangement

--- examples/romantic/floral_arrangement/main.go
+++ examples/romantic/floral_arrangement/main.go
@@ -1,15 +1,91 @@
 package main
 
 import (
+	"log"
+	"math"
+
+	"github.com/unixpickle/model3d/model2d"
 	"github.com/unixpickle/model3d/model3d"
 	"github.com/unixpickle/model3d/render3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 )
 
 func main() {
-	solid := model3d.JoinedSolid{
-		FlowerPot(),
-	}
+	log.Println("Creating flower pot...")
+	pot := FlowerPot()
+	potSDF := ColorFuncSDF(pot)
+
+	log.Println("Creating arrangement...")
+	potColor := toolbox3d.ConstantCoordColorFunc(render3d.NewColorRGB(0.85, 0.46, 0.24))
+	flowers, colorFunc := CompileArrangement(CreateArrangement(), potSDF, potColor)
+
+	log.Println("Creating mesh...")
+	solid := (model3d.JoinedSolid{
+		pot,
+		flowers,
+	}).Optimize()
 	mesh := model3d.MarchingCubesSearch(solid, 0.02, 8)
-	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 512, nil)
+
+	log.Println("Rendering...")
+	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 512, colorFunc.RenderColor)
+
+	log.Println("Saving...")
 	mesh.SaveGroupedSTL("arrangement.stl")
+}
+
+type FlowerDesc struct {
+	Base   model2d.Coord
+	Height float64
+	Flower *Flower
+}
+
+func CreateArrangement() []*FlowerDesc {
+	log.Println("Creating bermuda buttercup...")
+	flowerBB := NewBermudaButtercup()
+	log.Println("Creating rose...")
+	flowerRose := NewRose()
+	log.Println("Creating purple...")
+	flowerPurple := NewPurpleRowFlower()
+	log.Println("Done creating flowers.")
+	var results []*FlowerDesc
+	for i := 0; i < 3; i++ {
+		theta := float64(i) * 2 * math.Pi / 3
+		spacing := 2 * math.Pi / 6
+		results = append(results, &FlowerDesc{
+			Base:   model2d.XY(math.Cos(theta), math.Sin(theta)),
+			Height: 1.3,
+			Flower: flowerBB,
+		})
+		results = append(results, &FlowerDesc{
+			Base:   model2d.XY(math.Cos(theta+spacing), math.Sin(theta+spacing)),
+			Height: 1.7,
+			Flower: flowerRose,
+		})
+		results = append(results, &FlowerDesc{
+			Base:   model2d.XY(math.Cos(theta), math.Sin(theta)),
+			Height: 2.2,
+			Flower: flowerPurple,
+		})
+	}
+	return results
+}
+
+func CompileArrangement(fs []*FlowerDesc, colorFuncArgs ...interface{}) (model3d.Solid,
+	toolbox3d.CoordColorFunc) {
+	solids := model3d.JoinedSolid{}
+	stemColor := toolbox3d.ConstantCoordColorFunc(render3d.NewColorRGB(0, 0.8, 0))
+	for i, f := range fs {
+		log.Printf("Compiling flower %d/%d", i+1, len(fs))
+		stem := NewStem(model3d.XY(f.Base.X, f.Base.Y), f.Height, f.Flower.Tilt)
+		solids = append(solids, stem.Solid)
+		colorFuncArgs = append(colorFuncArgs, ColorFuncSDF(stem.Solid), stemColor)
+		flower := f.Flower.Place(stem.Tip)
+		solids = append(solids, flower.Solid)
+		colorFuncArgs = append(colorFuncArgs, flower.ColorSDF, flower.ColorFunc)
+	}
+	return solids.Optimize(), toolbox3d.JoinedCoordColorFunc(colorFuncArgs...).Cached()
+}
+
+func ColorFuncSDF(s model3d.Solid) model3d.SDF {
+	return model3d.MeshToSDF(model3d.MarchingCubesSearch(s, 0.025, 8))
 }
---
+++ examples/romantic/floral_arrangement/stem.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"math"
+
+	"github.com/unixpickle/model3d/model3d"
+	"github.com/unixpickle/model3d/toolbox3d"
+)
+
+const StemThickness = 0.2
+
+type Stem struct {
+	Solid model3d.Solid
+	Tip   model3d.Coord3D
+}
+
+func NewStem(baseCenter model3d.Coord3D, height, tilt float64) *Stem {
+	arcRadius := 0.5
+	offset := arcRadius * math.Sin(tilt)
+	segs := []model3d.Segment{}
+	segs = append(segs, model3d.NewSegment(model3d.Z(-StemThickness), model3d.Z(height-offset)))
+
+	pTheta := func(theta float64) model3d.Coord3D {
+		x := 1 + math.Cos(math.Pi-theta)
+		z := math.Sin(theta)
+		return model3d.XZ(x*arcRadius, z*arcRadius+height-offset)
+	}
+	eps := 0.01
+	for theta := 0.0; theta+eps < tilt; theta += eps {
+		segs = append(segs, model3d.NewSegment(pTheta(theta), pTheta(theta+eps)))
+	}
+	solid := toolbox3d.LineJoin(StemThickness, segs...)
+
+	// The tip of the stem is rounded and therefore goes
+	// further than the last segment.
+	finalPoint := pTheta(tilt)
+	finalDir := finalPoint.Sub(pTheta(tilt - eps)).Normalize()
+	tip := finalPoint.Add(finalDir.Scale(StemThickness))
+
+	xform := model3d.JoinedTransform{
+		model3d.Rotation(model3d.Z(1), math.Atan2(baseCenter.Y, baseCenter.X)),
+		&model3d.Translate{Offset: baseCenter},
+	}
+	return &Stem{
+		Solid: model3d.TransformSolid(xform, solid),
+		Tip:   xform.Apply(tip),
+	}
+}
