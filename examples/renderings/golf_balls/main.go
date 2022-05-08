4b9c27a40fdb0c9e0cd2dad70221302a2227e73f
Alex Nichol <unixpickle@gmail.com>
2022-05-08 11:13:15 -0400

    cleanup golf ball uniformity

--- examples/renderings/golf_balls/main.go
+++ examples/renderings/golf_balls/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math"
+
 	"github.com/unixpickle/model3d/model3d"
 	"github.com/unixpickle/model3d/render3d"
 )
@@ -14,15 +16,20 @@ func CreateGolfBall() *model3d.Mesh {
 	sphere := &model3d.Sphere{Radius: 1.0}
 
 	icosphere := model3d.NewMeshIcosphere(sphere.Center, sphere.Radius, 5)
+	coordTree := model3d.NewCoordTree(icosphere.VertexSlice())
 	dimples := model3d.JoinedSolid{}
 	for _, c := range icosphere.VertexSlice() {
-		dimples = append(dimples, &model3d.Sphere{Center: c.Scale(1.08), Radius: 0.15})
+		surfaceRadius := 0.5 * coordTree.KNN(2, c)[1].Dist(c)
+		radius := 0.2
+		// Find sin(theta) such that cos(theta)*radius = surfaceRadius
+		// cos(theta) = surfaceRadius/radius
+		// sin(theta) = sqrt(1 - (surfaceRadius/radius)^2)
+		offset := radius * math.Sqrt(1-math.Pow(surfaceRadius/radius, 2))
+		dimples = append(dimples, &model3d.Sphere{Center: c.Scale(1 + offset), Radius: radius})
 	}
 	subtracted := &model3d.SubtractedSolid{
 		Positive: sphere,
 		Negative: dimples.Optimize(),
 	}
-	fineMesh := model3d.MarchingCubesSearch(subtracted, 0.01, 8)
-	smoothSolid := model3d.NewColliderSolidInset(model3d.MeshToCollider(fineMesh), -0.05)
-	return model3d.MarchingCubesSearch(smoothSolid, 0.02, 8)
+	return model3d.MarchingCubesSearch(subtracted, 0.01, 8)
 }
