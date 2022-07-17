2227e5a4302e8aa6022bab26512a86f733dc3f66
Alex Nichol <unixpickle@gmail.com>
2022-07-17 11:26:03 -0700

    move search API to numerical library

---
+++ numerical/dense_search.go
@@ -0,0 +1,122 @@
+package numerical
+
+import (
+	"math"
+)
+
+// A LineSearch implements a 1D line search for minimizing
+// or maximizing 1D functions.
+//
+// The LineSearch is performed by first sampling coarsely
+// along the function, and then iteratively sampling more
+// densely around the current optimal solution.
+type LineSearch struct {
+	// Number of points to test along the input space.
+	Stops int
+
+	// Number of times to recursively search around the
+	// best point on the line.
+	Recursions int
+}
+
+// Minimize computes x and f(x) for x in [min, max] to
+// minimize f(x).
+func (l *LineSearch) Minimize(min, max float64, f func(float64) float64) (x, fVal float64) {
+	x, fVal = l.Maximize(min, max, func(x float64) float64 {
+		return -f(x)
+	})
+	return x, -fVal
+}
+
+// Maximize computes x and f(x) for x in [min, max] to
+// maximize f(x).
+func (l *LineSearch) Maximize(min, max float64, f func(float64) float64) (x, fVal float64) {
+	return l.maximize(min, max, f, l.Recursions)
+}
+
+func (l *LineSearch) maximize(min, max float64, f func(float64) float64,
+	recursions int) (float64, float64) {
+	if recursions < 0 {
+		panic("number of recursions cannot be negative")
+	}
+	size := max - min
+	xStep := size / float64(l.Stops)
+	solution := 0.0
+	value := math.Inf(-1)
+	for xi := 0; xi < l.Stops; xi++ {
+		x := float64(xi)*xStep + xStep/2 + min
+		v := f(x)
+		if v > value {
+			value = v
+			solution = x
+		}
+	}
+	if recursions == 0 {
+		return solution, value
+	}
+	newMin := math.Max(min, solution-xStep)
+	newMax := math.Min(max, solution+xStep)
+	return l.maximize(newMin, newMax, f, recursions-1)
+}
+
+// A GridSearch2D implements 2D grid search for minimizing
+// or maximizing 2D functions.
+type GridSearch2D struct {
+	// Number of points to test along x and y axes.
+	XStops int
+	YStops int
+
+	// Number of times to recursively search around the
+	// best point in the grid.
+	Recursions int
+}
+
+// Minimize uses grid search to find the minimum value of
+// f within the given bounds.
+//
+// Returns the point and its function value.
+func (g *GridSearch2D) Minimize(min, max Vec2,
+	f func(Vec2) float64) (Vec2, float64) {
+	c, v := g.Maximize(min, max, func(c Vec2) float64 {
+		return -f(c)
+	})
+	return c, -v
+}
+
+// Maximize uses grid search to find the maximum value of
+// f within the given bounds.
+//
+// Returns the point and its function value.
+func (g *GridSearch2D) Maximize(min, max Vec2, f func(Vec2) float64) (Vec2, float64) {
+	return g.maximize(min, max, f, g.Recursions)
+}
+
+func (g *GridSearch2D) maximize(min, max Vec2, f func(Vec2) float64, recursions int) (Vec2,
+	float64) {
+	if recursions < 0 {
+		panic("number of recursions cannot be negative")
+	}
+	size := max.Sub(min)
+	xStep := size[0] / float64(g.XStops)
+	yStep := size[1] / float64(g.YStops)
+	solution := Vec2{}
+	value := math.Inf(-1)
+	for xi := 0; xi < g.XStops; xi++ {
+		x := float64(xi)*xStep + xStep/2 + min[0]
+		for yi := 0; yi < g.YStops; yi++ {
+			y := float64(yi)*yStep + yStep/2 + min[1]
+			c := Vec2{x, y}
+			v := f(c)
+			if v > value {
+				value = v
+				solution = c
+			}
+		}
+	}
+	if recursions == 0 {
+		return solution, value
+	}
+	newMin := min.Max(solution.Sub(Vec2{xStep, yStep}))
+	newMax := max.Min(solution.Add(Vec2{xStep, yStep}))
+	return g.maximize(newMin, newMax, f, recursions-1)
+}
--- toolbox3d/min_max.go
+++ toolbox3d/min_max.go
@@ -1,56 +1,20 @@
 package toolbox3d
 
 import (
-	"math"
-
 	"github.com/unixpickle/model3d/model2d"
+	"github.com/unixpickle/model3d/numerical"
 )
 
-// A LineSearch implements a 1D line search for minimizing
-// or maximizing 1D functions.
-type LineSearch struct {
-	// Number of points to test along the input space.
-	Stops int
-
-	// Number of times to recursively search around the
-	// best point on the line.
-	Recursions int
-}
+// LineSearch is an extension of numerical.LineSearch with
+// extra helper methods for concrete applications.
+type LineSearch numerical.LineSearch
 
 func (l *LineSearch) Minimize(min, max float64, f func(float64) float64) (x, fVal float64) {
-	x, fVal = l.Maximize(min, max, func(x float64) float64 {
-		return -f(x)
-	})
-	return x, -fVal
+	return (*numerical.LineSearch)(l).Minimize(min, max, f)
 }
 
 func (l *LineSearch) Maximize(min, max float64, f func(float64) float64) (x, fVal float64) {
-	return l.maximize(min, max, f, l.Recursions)
-}
-
-func (l *LineSearch) maximize(min, max float64, f func(float64) float64,
-	recursions int) (float64, float64) {
-	if recursions < 0 {
-		panic("number of recursions cannot be negative")
-	}
-	size := max - min
-	xStep := size / float64(l.Stops)
-	solution := 0.0
-	value := math.Inf(-1)
-	for xi := 0; xi < l.Stops; xi++ {
-		x := float64(xi)*xStep + xStep/2 + min
-		v := f(x)
-		if v > value {
-			value = v
-			solution = x
-		}
-	}
-	if recursions == 0 {
-		return solution, value
-	}
-	newMin := math.Max(min, solution-xStep)
-	newMax := math.Min(max, solution+xStep)
-	return l.maximize(newMin, newMax, f, recursions-1)
+	return (*numerical.LineSearch)(l).Maximize(min, max, f)
 }
 
 // CurveBounds approximates the bounding box of a
@@ -74,67 +38,26 @@ func (l *LineSearch) CurveBounds(min, max float64, f func(float64) model2d.Coord
 	return model2d.NewCoordArray(minArr), model2d.NewCoordArray(maxArr)
 }
 
-// A GridSearch2D implements 2D grid search for minimizing
-// or maximizing 2D functions.
-type GridSearch2D struct {
-	// Number of points to test along x and y axes.
-	XStops int
-	YStops int
+// GridSearch2D is an extension of numerical.GridSearch2D
+// with extra helper methods for concrete applications.
+type GridSearch2D numerical.GridSearch2D
 
-	// Number of times to recursively search around the
-	// best point in the grid.
-	Recursions int
-}
-
-// Minimize uses grid search to find the minimum value of
-// f within the given bounds.
-//
-// Returns the point and its function value.
 func (g *GridSearch2D) Minimize(min, max model2d.Coord,
 	f func(model2d.Coord) float64) (model2d.Coord, float64) {
-	c, v := g.Maximize(min, max, func(c model2d.Coord) float64 {
-		return -f(c)
-	})
-	return c, -v
+	x, y := (*numerical.GridSearch2D)(g).Minimize(min.Array(), max.Array(),
+		func(c numerical.Vec2) float64 {
+			return f(model2d.NewCoordArray(c))
+		})
+	return model2d.NewCoordArray(x), y
 }
 
-// Maximize uses grid search to find the maximum value of
-// f within the given bounds.
-//
-// Returns the point and its function value.
 func (g *GridSearch2D) Maximize(min, max model2d.Coord,
 	f func(model2d.Coord) float64) (model2d.Coord, float64) {
-	return g.maximize(min, max, f, g.Recursions)
-}
-
-func (g *GridSearch2D) maximize(min, max model2d.Coord,
-	f func(model2d.Coord) float64, recursions int) (model2d.Coord, float64) {
-	if recursions < 0 {
-		panic("number of recursions cannot be negative")
-	}
-	size := max.Sub(min)
-	xStep := size.X / float64(g.XStops)
-	yStep := size.Y / float64(g.YStops)
-	solution := model2d.Coord{}
-	value := math.Inf(-1)
-	for xi := 0; xi < g.XStops; xi++ {
-		x := float64(xi)*xStep + xStep/2 + min.X
-		for yi := 0; yi < g.YStops; yi++ {
-			y := float64(yi)*yStep + yStep/2 + min.Y
-			c := model2d.XY(x, y)
-			v := f(c)
-			if v > value {
-				value = v
-				solution = c
-			}
-		}
-	}
-	if recursions == 0 {
-		return solution, value
-	}
-	newMin := min.Max(solution.Sub(model2d.XY(xStep, yStep)))
-	newMax := max.Min(solution.Add(model2d.XY(xStep, yStep)))
-	return g.maximize(newMin, newMax, f, recursions-1)
+	x, y := (*numerical.GridSearch2D)(g).Maximize(min.Array(), max.Array(),
+		func(c numerical.Vec2) float64 {
+			return f(model2d.NewCoordArray(c))
+		})
+	return model2d.NewCoordArray(x), y
 }
 
 // MaxSDF finds the point with maximal SDF and returns it,
