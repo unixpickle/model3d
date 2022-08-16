c7d54efcb43c4bba8cf481821711ea7c2fa0a617
Alex Nichol <unixpickle@gmail.com>
2022-08-15 20:50:52 -0400

    cable car structure

--- examples/parody/cable_car/main.go
+++ examples/parody/cable_car/main.go
@@ -3,36 +3,67 @@ package main
 import (
 	"math"
 
+	"github.com/unixpickle/model3d/model2d"
 	"github.com/unixpickle/model3d/model3d"
 	"github.com/unixpickle/model3d/render3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 )
 
 const (
-	AxleRadius         = 0.15
-	BaseThickness      = 0.1
-	WheelThickness     = 0.15
-	WheelRadius        = 0.35
-	WheelSlip          = 0.05
-	ArchPanelWidth     = 1.0
-	ArchPanelHeight    = 3.0
-	ArchPanelThickness = 0.1
+	AxleRadius           = 0.15
+	SideOverhang         = 0.1
+	TopBottomThickness   = 0.15
+	WheelThickness       = 0.15
+	WheelRadius          = 0.35
+	WheelSlip            = 0.05
+	ArchInset            = 0.16
+	ArchTopInset         = 0.2
+	ArchPanelWidth       = 1.0
+	ArchPanelHeight      = 2.6
+	ArchPanelThickness   = 0.1
+	ArchOutlineThickness = 0.025
+	TopLipSpace          = 0.02
+	ArchedTopHeight      = 0.5
 )
 
+var (
+	BottomColor      = render3d.NewColor(0.1)
+	CarColor         = render3d.NewColorRGB(1.0, 0.3, 0.3)
+	ArchOutlineColor = render3d.NewColorRGB(1.0, 1.0, 0.0)
+)
+
+type ModelFn func() (model3d.Solid, toolbox3d.CoordColorFunc)
+
 func main() {
-	model := model3d.JoinedSolid{BasePlatform(), ArchSides()}
-	mesh := model3d.MarchingCubesSearch(model, 0.02, 8)
-	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 300, nil)
+	fns := []ModelFn{
+		BaseWheels,
+		TopPanel,
+		BottomPanel,
+		ArchSides,
+		ArchedTop,
+	}
+	mesh, colorFn := BuildMesh(fns)
+	render3d.SaveRandomGrid("rendering.png", mesh, 3, 3, 300, colorFn.RenderColor)
 }
 
-func BasePlatform() model3d.Solid {
-	base := model3d.NewRect(
-		model3d.XYZ(-3.5, -1.5, -BaseThickness),
-		model3d.XYZ(3.5, 1.5, 1e-5),
-	)
+func BuildMesh(fns []ModelFn) (*model3d.Mesh, toolbox3d.CoordColorFunc) {
+	var solids model3d.JoinedSolid
+	solidsAndColors := []any{}
+	for _, partFn := range fns {
+		solid, colorFn := partFn()
+		solids = append(solids, solid)
+		solidsAndColors = append(solidsAndColors, solid, colorFn)
+	}
+	mesh, interior := model3d.MarchingCubesInterior(solids.Optimize(), 0.02, 8)
+	colorFn := toolbox3d.JoinedSolidCoordColorFunc(interior, solidsAndColors...)
+	return mesh, colorFn
+}
+
+func BaseWheels() (model3d.Solid, toolbox3d.CoordColorFunc) {
 	var wheels model3d.JoinedSolid
 	firstX := 3.5 - WheelRadius*2
 	for _, x := range []float64{-firstX, -firstX + WheelRadius*2.5, firstX - WheelRadius*2.5, firstX} {
-		wheelZ := -(BaseThickness + WheelRadius - WheelSlip)
+		wheelZ := -(TopBottomThickness + WheelRadius - WheelSlip)
 		for _, y := range []float64{-1.3, 1.3} {
 			wheel := &model3d.Cylinder{
 				P1:     model3d.XYZ(x, y-WheelThickness/2, wheelZ),
@@ -49,30 +80,120 @@ func BasePlatform() model3d.Solid {
 			},
 			model3d.NewRect(
 				model3d.XYZ(x-AxleRadius, -1.3, wheelZ),
-				model3d.XYZ(x+AxleRadius, 1.3, -BaseThickness+1e-5),
+				model3d.XYZ(x+AxleRadius, 1.3, -TopBottomThickness+1e-5),
 			),
 		}
 		wheels = append(wheels, axle...)
 	}
-	return append(wheels, base).Optimize()
+	return wheels.Optimize(), toolbox3d.ConstantCoordColorFunc(BottomColor)
 }
 
-func ArchSides() model3d.Solid {
+func TopPanel() (model3d.Solid, toolbox3d.CoordColorFunc) {
+	// Create a lip so that this lid slides into the base
+	// of the structure but doesn't move around too much.
+	sliceZ := ArchPanelHeight - 1e-5
+	solid3d, _ := ArchSides()
+	solid2d := model2d.CheckedFuncSolid(
+		solid3d.Min().XY(),
+		solid3d.Max().XY(),
+		func(c model2d.Coord) bool {
+			return solid3d.Contains(model3d.XYZ(c.X, c.Y, sliceZ))
+		},
+	)
+	outline2d := model2d.MarchingSquaresSearch(solid2d, 0.005, 8)
+	mesh := model2d.MeshToHierarchy(outline2d)[0].Children[0].Mesh
+	collider2d := model2d.MeshToCollider(mesh)
+	lip2d := &model2d.SubtractedSolid{
+		Positive: model2d.NewColliderSolidInset(collider2d, TopLipSpace),
+		Negative: model2d.NewColliderSolidInset(collider2d, TopLipSpace+0.1),
+	}
+	lip3d := model3d.ProfileSolid(lip2d, sliceZ-0.1, ArchPanelHeight+1e-5)
+	topSolid, colorFn := topBottomPanel(true)
+	return model3d.JoinedSolid{topSolid, lip3d}, func(c model3d.Coord3D) render3d.Color {
+		if lip3d.Contains(c) {
+			return CarColor
+		}
+		return colorFn(c)
+	}
+}
+
+func BottomPanel() (model3d.Solid, toolbox3d.CoordColorFunc) {
+	return topBottomPanel(false)
+}
+
+func topBottomPanel(top bool) (model3d.Solid, toolbox3d.CoordColorFunc) {
+	// Ends are rounded according to a virtual circle
+	// with the given radius. Higher radii mean less
+	// curvature.
+	virtualRadius := 5.0
+	maxDim := 4.0
+	base := model3d.CheckedFuncSolid(
+		model3d.XYZ(-maxDim, -1.5-SideOverhang, -TopBottomThickness-1e-5),
+		model3d.XYZ(maxDim, 1.5+SideOverhang, 1e-5),
+		func(c model3d.Coord3D) bool {
+			if c.X < 0 {
+				return c.XY().Dist(model2d.X(virtualRadius-maxDim)) < virtualRadius
+			} else {
+				return c.XY().Dist(model2d.X(-virtualRadius+maxDim)) < virtualRadius
+			}
+		},
+	)
+	if top {
+		return model3d.TranslateSolid(base, model3d.Z(ArchPanelHeight+TopBottomThickness)),
+			toolbox3d.ConstantCoordColorFunc(CarColor)
+	} else {
+		return base, toolbox3d.ConstantCoordColorFunc(BottomColor)
+	}
+}
+
+func ArchSides() (model3d.Solid, toolbox3d.CoordColorFunc) {
+	baseSolid := outsetArchSides(0)
+	midOutset := outsetArchSides(ArchOutlineThickness)
+	fullOutset := outsetArchSides(ArchOutlineThickness * 2)
+	return baseSolid, func(c model3d.Coord3D) render3d.Color {
+		if !fullOutset.Contains(c) && midOutset.Contains(c) {
+			return ArchOutlineColor
+		} else if math.Abs(c.Y) < 1.5-ArchPanelThickness {
+			return CarColor
+		} else {
+			return BottomColor
+		}
+	}
+}
+
+func outsetArchSides(outset float64) model3d.Solid {
 	var result model3d.JoinedSolid
 	for i := 0; i < 7; i++ {
 		x := -3.5 + ArchPanelWidth/2 + float64(i)*ArchPanelWidth
 		fullHeight := i/3%2 == 0
 		for _, y := range []float64{-1.5 + ArchPanelThickness/2, 1.5 - ArchPanelThickness/2} {
-			result = append(result, ArchPanel(model3d.XYZ(x, y, ArchPanelHeight/2), fullHeight))
+			result = append(
+				result,
+				ArchPanel(
+					model3d.XYZ(x, y, ArchPanelHeight/2),
+					fullHeight,
+					outset,
+				),
+			)
 		}
 	}
 
 	var uncurvedPanel model3d.JoinedSolid
 	for i := 0; i < 3; i++ {
-		uncurvedPanel = append(uncurvedPanel, ArchPanel(
-			model3d.XYZ(ArchPanelWidth*float64(i), 3.5-ArchPanelWidth, ArchPanelHeight/2),
-			false,
-		))
+		// Shrink the two angled arches to prevent them
+		// from coming close to the sides of the car.
+		extraOutset := -0.03
+		if i == 1 {
+			extraOutset = 0
+		}
+		uncurvedPanel = append(
+			uncurvedPanel,
+			ArchPanel(
+				model3d.XYZ(ArchPanelWidth*(0.5+float64(i)), 3.5, ArchPanelHeight/2),
+				false,
+				outset+extraOutset,
+			),
+		)
 	}
 	curveOffset := 0.2
 	curvedPanel := model3d.CheckedFuncSolid(
@@ -100,28 +221,64 @@ func ArchSides() model3d.Solid {
 	return result
 }
 
-func ArchPanel(center model3d.Coord3D, fullHeight bool) model3d.Solid {
+func ArchPanel(center model3d.Coord3D, fullHeight bool, outset float64) model3d.Solid {
 	outer := model3d.NewRect(
 		model3d.XYZ(-ArchPanelWidth/2, -ArchPanelThickness/2, -ArchPanelHeight/2),
 		model3d.XYZ(ArchPanelWidth/2, ArchPanelThickness/2, ArchPanelHeight/2),
 	)
-	inset := 0.1
+	topInset := ArchTopInset - outset
+	inset := ArchInset - outset
 	archRadius := ArchPanelWidth/2 - inset
 	arch := &model3d.Cylinder{
-		P1:     model3d.XYZ(0, -ArchPanelThickness-1e-5, ArchPanelHeight/2-(inset+archRadius)),
-		P2:     model3d.XYZ(0, ArchPanelThickness+1e-5, ArchPanelHeight/2-(inset+archRadius)),
+		P1:     model3d.XYZ(0, -ArchPanelThickness-1e-5, ArchPanelHeight/2-(topInset+archRadius)),
+		P2:     model3d.XYZ(0, ArchPanelThickness+1e-5, ArchPanelHeight/2-(topInset+archRadius)),
 		Radius: archRadius,
 	}
 	archBottom := model3d.NewRect(
 		model3d.XYZ(-archRadius, -ArchPanelThickness-1e-5, -ArchPanelHeight/2+inset),
-		model3d.XYZ(-archRadius, ArchPanelThickness+1e-5, ArchPanelHeight/2-(inset+archRadius)+0.1),
+		model3d.XYZ(archRadius, ArchPanelThickness+1e-5, ArchPanelHeight/2-(topInset+archRadius)+0.1),
 	)
 	if !fullHeight {
 		archBottom.MinVal.Z = 0
 	}
+	archBottom.MinVal.Z -= outset
 	untranslated := &model3d.SubtractedSolid{
 		Positive: outer,
 		Negative: model3d.JoinedSolid{arch, archBottom},
 	}
 	return model3d.TranslateSolid(untranslated, center)
+}
+
+func ArchedTop() (model3d.Solid, toolbox3d.CoordColorFunc) {
+	virtualRadius := 8.0
+	minZ := ArchPanelHeight + TopBottomThickness - 1e-5
+	maxZ := minZ + ArchedTopHeight
+
+	outerCurve := model3d.Cylinder{
+		P1:     model3d.XYZ(-2.5, 0, maxZ-virtualRadius),
+		P2:     model3d.XYZ(2.5, 0, maxZ-virtualRadius),
+		Radius: virtualRadius,
+	}
+	innerCurve := outerCurve
+	innerCurve.Radius -= TopBottomThickness
+
+	bounds := model3d.NewRect(
+		model3d.XYZ(-2.5, -1.0, minZ),
+		model3d.XYZ(2.5, 1.0, maxZ),
+	)
+	x1 := 0.6
+	x2 := 0.6 + TopBottomThickness
+	return model3d.IntersectedSolid{
+		model3d.JoinedSolid{
+			model3d.IntersectedSolid{
+				model3d.JoinedSolid{
+					model3d.NewRect(model3d.XYZ(-2.5, -x2, minZ), model3d.XYZ(2.5, -x1, maxZ)),
+					model3d.NewRect(model3d.XYZ(-2.5, x1, minZ), model3d.XYZ(2.5, x2, maxZ)),
+				},
+				&outerCurve,
+			},
+			&model3d.SubtractedSolid{Positive: &outerCurve, Negative: &innerCurve},
+		},
+		bounds,
+	}, toolbox3d.ConstantCoordColorFunc(CarColor)
 }
